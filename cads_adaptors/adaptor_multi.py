1f98ad6677b52fa6d74a31d658f096dce7a31da5
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-06-26 10:22:58 +0100

    multi-adaptor added

---
+++ cads_adaptors/adaptor_multi.py
@@ -0,0 +1,74 @@
+# WORK IN PROCESS
+from typing import Any
+
+# import os
+
+from cads_adaptors import AbstractCdsAdaptor
+from cads_adaptors.adaptor import Request
+
+
+class MultiAdaptor(AbstractCdsAdaptor):
+    @staticmethod
+    def split_request(
+        full_request: Request,   # User request
+        this_values: dict[str, Any],    # key: [values] for the adaptor component
+        **config: Any,
+    ) -> Request:
+        """
+        This basic request splitter, splits based on whether the values are relevant to
+        the specific adaptor.
+        More complex constraints may need a more detailed splitter
+        """        
+        this_request = {}
+        for key, vals in full_request.items():
+            this_request[key] = [v for v in vals if v in this_values.get(key, [])]
+        
+        # Check all required keys exist:
+        if not all([key in this_request for key in config.get("required_keys", [])]):
+            return {}
+        
+        return this_request
+
+    @staticmethod
+    def merge_results(results: list):
+        """
+        This basic results merge, creates a zip file containing all results.
+        """       
+        import zipfile
+
+        base_target = str(hash(tuple(results)))
+
+        target = f"{base_target}.zip"
+
+        with zipfile.ZipFile(target, mode="w") as archive:
+            for p in results:
+                archive.write(p)
+
+        # for p in results:
+        #     os.remove(p)
+
+        return target
+
+    def __init__(self, form: dict[str, Any], **config: Any):
+        from cads_adaptors.tools import adaptor_tools
+
+        super().__init__(form, **config)
+        self.adaptors = {}
+        self.values = {}
+        for adaptor_tag, adaptor_desc in config["adaptors"].items():
+            self.adaptors[adaptor_tag] = adaptor_tools.get_adaptor(adaptor_desc, form)
+            self.values[adaptor_tag] = adaptor_desc.get("values", {})
+
+    def retrieve(self, request: Request):
+        results = []
+        for adaptor_tag, this_adaptor in self.adaptors.items():
+            this_request = self.split_request(request, self.values[adaptor_tag], **self.config)
+
+            # TODO: check this_request is valid for this_adaptor, or rely on try?
+            try:
+                results.append(this_adaptor.retrieve(this_request))
+            except:
+                pass
+
+        return self.merge_results(results)
+
