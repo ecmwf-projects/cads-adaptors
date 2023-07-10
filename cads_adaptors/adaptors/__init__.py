68fc77ff590086ca588a6eafcc5cc8e8d60baa4f
James Varndell <james.varndell@ecmwf.int>
2023-07-10 17:31:59 +0100

    Removes AbstractCdsAdaptor from top level (moved to cds.py)

--- cads_adaptors/adaptors/__init__.py
+++ cads_adaptors/adaptors/__init__.py
@@ -75,27 +75,3 @@ class DummyAdaptor(AbstractAdaptor):
                     fp.write(random.read(length))
                     size -= length
         return open("dummy.grib", "rb")
-
-
-class AbstractCdsAdaptor(AbstractAdaptor):
-    resources = {"CADS_ADAPTORS": 1}
-
-    def __init__(self, form: dict[str, Any], **config: Any):
-        self.form = form
-        self.constraints = config.pop("constraints", [])
-        self.mapping = config.pop("mapping", {})
-        self.licences: list[tuple[str, int]] = config.pop("licences", [])
-        self.config = config
-
-    def validate(self, request: Request) -> bool:
-        return True
-
-    def apply_constraints(self, request: Request) -> dict[str, Any]:
-        return constraints.validate_constraints(self.form, request, self.constraints)
-
-    def estimate_costs(self, request: Request) -> dict[str, int]:
-        costs = {"size": costing.estimate_size(self.form, request, self.constraints)}
-        return costs
-
-    def get_licences(self, request: Request) -> list[tuple[str, int]]:
-        return self.licences
