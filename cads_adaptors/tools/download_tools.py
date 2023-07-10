347fc1c7a2d79956243cb417d0f81721382f75c6
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-07-10 12:10:40 +0100

    type checking fixed

--- cads_adaptors/tools/download_tools.py
+++ cads_adaptors/tools/download_tools.py
@@ -1,11 +1,12 @@
+import operator
 import os
-from typing import List
+from typing import List, BinaryIO, Dict, Callable
 
 from cads_adaptors.tools import ensure_list
 
 
 # TODO use targzstream
-def zip_paths(paths: List[str], base_target: str = "output-data", **kwargs) -> str:
+def zip_paths(paths: List[str], base_target: str = "output-data", **kwargs) -> BinaryIO:
     import zipfile
 
     target = f"{base_target}.zip"
@@ -24,7 +25,7 @@ def targz_paths(
     paths: List[str],
     base_target: str = "output-data",
     **kwargs,
-) -> str:
+) -> BinaryIO:
     import tarfile
 
     target = f"{base_target}.tar.gz"
@@ -41,16 +42,16 @@ def targz_paths(
 def list_paths(
     paths: List[str],
     **kwargs,
-):
+) -> List:
     return [open(path, "rb") for path in ensure_list(paths)]
 
 
-def raw_path(paths: List[str], **kwargs):
+def raw_path(paths: List[str], **kwargs) -> BinaryIO:
     assert len(paths) == 1
     return open(paths[0], "rb")
 
 
-DOWNLOAD_FORMATS = {
+DOWNLOAD_FORMATS: Dict[str, Callable] = {
     "zip": zip_paths,
     "tgz": targz_paths,
     "list": list_paths,
