6ec6e56530427800e1a9572abf74abcc8735e88e
James Varndell <36902610+JamesVarndell@users.noreply.github.com>
2023-07-11 09:30:33 +0100

    Refactor to create a more maintainable structure (#16)
    * Refactor to create a more maintainable structure
    
    * Renames adaptors back to original names for compatibility with the rest of the stack
    
    * Adds comments around blocks to be deprecated
    
    * Renames refereance to removed AbstractAdaptor class
    
    * Renames adaptor classes back to original names
    
    * Moved AbstractCdsAdaptor
    
    * Removes AbstractCdsAdaptor from top level (moved to cds.py)
    
    * updating actions
    
    * QA tweaks
    
    * Rverts naming of base abstract adaptor class
    
    * Renames MARS and URL adaptors
    
    ---------
    
    Co-authored-by: EddyCMWF <Edward.Comyn-Platt@ecmwf.int>

---
+++ cads_adaptors/adaptors/mars.py
@@ -0,0 +1,40 @@
+import os
+from typing import BinaryIO
+
+from cads_adaptors import mapping
+from cads_adaptors.adaptors import Request, cds
+
+
+class DirectMarsCdsAdaptor(cds.AbstractCdsAdaptor):
+    resources = {"MARS_CLIENT": 1}
+
+    def retrieve(self, request: Request) -> BinaryIO:
+        import subprocess
+
+        with open("r", "w") as fp:
+            print("retrieve, target=data.grib", file=fp)
+            for key, value in request.items():
+                if not isinstance(value, (list, tuple)):
+                    value = [value]
+                print(f", {key}={'/'.join(str(v) for v in value)}", file=fp)
+
+        env = dict(**os.environ)
+        # FIXME: set with the namespace and user_id
+        namespace = "cads"
+        user_id = 0
+        env["MARS_USER"] = f"{namespace}-{user_id}"
+
+        subprocess.run(["/usr/local/bin/mars", "r"], check=True, env=env)
+
+        return open("data.grib")  # type: ignore
+
+
+class MarsCdsAdaptor(cds.AbstractCdsAdaptor):
+    def retrieve(self, request: Request) -> BinaryIO:
+        format = request.pop("format", "grib")
+
+        mapped_request = mapping.apply_mapping(request, self.mapping)  # type: ignore
+        if format != "grib":
+            # FIXME: reformat if needed
+            pass
+        return super().retrieve(mapped_request)
