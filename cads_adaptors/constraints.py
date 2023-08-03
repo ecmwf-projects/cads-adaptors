5cdf8d0451bdc687d75e5d02fec9ce84a671a925
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2023-08-03 15:55:40 +0100

    Revert to the JS-inspired implementation

--- cads_adaptors/constraints.py
+++ cads_adaptors/constraints.py
@@ -168,16 +168,9 @@ def get_possible_values(
 
     """
     result: dict[str, set[Any]] = {key: set() for key in form}
-    #  ECP testing
-    # constraints should only be applied to keys that are in all constraints cubes
-    use_keys = [
-        key for key in list(selection) if all([key in list(cube) for cube in constraints])
-    ]
-    print(use_keys)
     for combination in constraints:
         ok = True
-        for field_name in use_keys:
-            selected_values = selection[field_name]
+        for field_name, selected_values in selection.items():
             if field_name in combination.keys():
                 if len(selected_values & combination[field_name]) == 0:
                     ok = False
@@ -393,7 +386,7 @@ def validate_constraints(
     constraints = remove_unsupported_vars(constraints, unsupported_vars)
     selection = parse_selection(request["inputs"], unsupported_vars)
 
-    return apply_constraints(parsed_form, selection, constraints)
+    return apply_constraints_v2(parsed_form, selection, constraints)
 
 
 def get_keys(constraints: list[dict[str, Any]]) -> set[str]:
