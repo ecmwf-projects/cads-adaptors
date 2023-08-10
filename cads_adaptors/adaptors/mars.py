f8a198d81af227c7c9e02b69d9f6b142d1406d33
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-08-10 15:42:09 +0100

    mars adaptor back to main

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -3,7 +3,6 @@ from typing import BinaryIO
 
 from cads_adaptors import mapping
 from cads_adaptors.adaptors import Request, cds
-from cads_adaptors.tools import ensure_list
 
 
 class DirectMarsCdsAdaptor(cds.AbstractCdsAdaptor):
@@ -11,18 +10,13 @@ class DirectMarsCdsAdaptor(cds.AbstractCdsAdaptor):
 
     def retrieve(self, request: Request) -> BinaryIO:
         import subprocess
-        
-        request = ensure_list(request)
 
         with open("r", "w") as fp:
-            for i, req in enumerate(request):
-                print("retrieve,", file=fp)
-                if i==0:
-                    print("target=data.grib", file=fp)
-                for key, value in req.items():
-                    if not isinstance(value, (list, tuple)):
-                        value = [value]
-                    print(f", {key}={'/'.join(str(v) for v in value)}", file=fp)
+            print("retrieve, target=data.grib", file=fp)
+            for key, value in request.items():
+                if not isinstance(value, (list, tuple)):
+                    value = [value]
+                print(f", {key}={'/'.join(str(v) for v in value)}", file=fp)
 
         env = dict(**os.environ)
         # FIXME: set with the namespace and user_id
