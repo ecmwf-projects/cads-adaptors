43778bd4f8172c1817f63292b949fa94a561d586
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-08-28 18:00:04 +0200

    qa

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -2,7 +2,7 @@ import os
 from typing import BinaryIO
 
 from cads_adaptors import mapping
-from cads_adaptors.adaptors import Context, Request, cds
+from cads_adaptors.adaptors import Request, cds
 
 
 def execute_mars(request: Request, target="data.grib"):
