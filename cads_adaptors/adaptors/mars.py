ca5c2e76415d02d77b4fd89cdb7a23e9edb07b8e
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-09-28 12:59:26 +0100

    MultiMarsCdsAdaptor (#36)
    * MultiMarsCdsAdaptor added, some removal of old comments
    
    * qa
    
    * correct typing execute_mars method
    
    * update to execute_mars to accept lists
    
    * correction to multi-adaptor exception logs

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -1,19 +1,25 @@
 import os
-from typing import BinaryIO
+from typing import BinaryIO, Union
 
 from cads_adaptors import mapping
 from cads_adaptors.adaptors import Request, cds
+from cads_adaptors.tools.general import ensure_list
 
 
-def execute_mars(request: Request, target="data.grib"):
+def execute_mars(request: Union[Request, list], target="data.grib"):
     import subprocess
 
+    requests = ensure_list(request)
     with open("r", "w") as fp:
-        print(f"retrieve, target={target}", file=fp)
-        for key, value in request.items():
-            if not isinstance(value, (list, tuple)):
-                value = [value]
-            print(f", {key}={'/'.join(str(v) for v in value)}", file=fp)
+        for i, req in enumerate(requests):
+            print("retrieve", file=fp)
+            # Add target file to first request, any extra store in same grib
+            if i == 0:
+                print(f", target={target}", file=fp)
+            for key, value in req.items():
+                if not isinstance(value, (list, tuple)):
+                    value = [value]
+                print(f", {key}={'/'.join(str(v) for v in value)}", file=fp)
 
     env = dict(**os.environ)
     # FIXME: set with the namespace and user_id
