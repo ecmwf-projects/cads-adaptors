9bcaa522f1ceb4d0b6732e36e2ac2de2920c92cf
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-10-02 16:54:14 +0100

    call workflow explicitly

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -3,7 +3,7 @@ import socket
 from typing import BinaryIO
 
 from cads_adaptors.adaptors.cds import AbstractCdsAdaptor, Request
-from cads_adaptors.tools import logger
+from cads_adaptors.tools.logger import logger
 
 os.environ["ROOK_URL"] = "http://compute.mips.copernicus-climate.eu/wps"
 
