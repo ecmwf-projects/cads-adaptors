c389724056a0394d3b320c0b5e57dff24f3a7255
Mattia Almansi <m.almansi@bopen.eu>
2023-10-20 15:07:23 +0200

    refactor execute mars to update context (#57)
    * refactor execute mars to update context
    
    * check r content
    
    * better tests
    
    * return target only

---
+++ tests/test_15_mars.py
@@ -0,0 +1,24 @@
+import pytest
+
+from cads_adaptors.adaptors import mars
+
+
+@pytest.mark.parametrize(
+    "cmd,error_msg",
+    (
+        ("cat r; echo error 1>&2; exit 1", "MARS has crashed."),
+        ("cat r; touch data.grib; echo error 1>&2", "MARS returned no data."),
+    ),
+)
+def test_execute_mars_errors(tmp_path, monkeypatch, cmd, error_msg):
+    monkeypatch.chdir(tmp_path)  # execute_mars generates files in the working dir
+    context = mars.Context()
+    with pytest.raises(RuntimeError, match=error_msg):
+        mars.execute_mars(
+            {},
+            context=context,
+            mars_cmd=("bash", "-c", cmd),
+        )
+    assert context.stdout == "retrieve\n, target=data.grib\n"
+    assert context.stderr == "error\n"
+    assert context.user_visible_log == "retrieve\n, target=data.grib\n"
