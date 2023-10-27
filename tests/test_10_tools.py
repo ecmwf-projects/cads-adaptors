31f87a6bf907115190e1554365e00ec1be1ca9d0
Mattia Almansi <m.almansi@bopen.eu>
2023-10-27 19:32:43 +0200

    fix test

--- tests/test_10_tools.py
+++ tests/test_10_tools.py
@@ -5,13 +5,14 @@ import time
 import fsspec
 import pytest
 
-from cads_adaptors import tools
+from cads_adaptors.tools.url_tools import try_download
 
 
 @pytest.fixture()
 def ftp(tmp_path):
     ftp_path = tmp_path / "ftp"
-    ftp_path.mkdir()
+    (ftp_path / "test").mkdir(parents=True)
+    (ftp_path / "test" / "foo").write_bytes(b"foo")
     P = subprocess.Popen(
         [sys.executable, "-m", "pyftpdlib", "-d", str(ftp_path)],
         stderr=subprocess.STDOUT,
@@ -30,8 +31,12 @@ def test_multiurl(tmp_path, monkeypatch, ftp):
 
     host, port = ftp
     fs = fsspec.filesystem("ftp", host=host, port=port)
-    fs.pipe("/test-file", b"dummy")
+    fs.download("test/foo", "foo-fsspec")
+    assert (tmp_path / "foo-fsspec").read_bytes() == b"foo"
 
-    urls = [f"ftp://{host}:{port}/test-file"]
-    tools.url_tools.try_download(urls)
+    url = f"ftp://{host}:{port}/test/foo"
+    subprocess.run(("wget", url, "-O", "foo-wget"), check=True)
+    assert (tmp_path / "foo-wget").read_bytes() == b"foo"
+
+    try_download([url])
     # TODO: add assertions
