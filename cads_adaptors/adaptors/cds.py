2cd57821bbddf60cda5c4b4a4d6a170052036f7b
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-11-30 09:56:28 +0000

    Include receipts in downloads and refactor of common code (#67)
    * make_receipt method added
    * Refactor
    * default receipt=False
    * defaults from CDS
    * warning for bad format requested, allow format conversion kwargs

--- cads_adaptors/adaptors/cds.py
+++ cads_adaptors/adaptors/cds.py
@@ -1,7 +1,10 @@
-from typing import Any
+import os
+from copy import deepcopy
+from typing import Any, Union
 
-from cads_adaptors import constraints, costing
+from cads_adaptors import constraints, costing, mapping
 from cads_adaptors.adaptors import AbstractAdaptor, Context, Request
+from cads_adaptors.tools.general import ensure_list
 
 
 class AbstractCdsAdaptor(AbstractAdaptor):
@@ -15,6 +18,11 @@ class AbstractCdsAdaptor(AbstractAdaptor):
         self.licences: list[tuple[str, int]] = config.pop("licences", [])
         self.config = config
         self.context = Context()
+        # The following attributes are updated during the retireve method
+        self.input_request: Request = Request()
+        self.mapped_request: Request = Request()
+        self.download_format: str = "zip"
+        self.receipt: bool = False
 
     def validate(self, request: Request) -> bool:
         return True
@@ -28,3 +36,88 @@ class AbstractCdsAdaptor(AbstractAdaptor):
 
     def get_licences(self, request: Request) -> list[tuple[str, int]]:
         return self.licences
+
+    # This is essentially a second __init__, but only for when we have a request at hand
+    # and currently only implemented for retrieve methods
+    def _pre_retrieve(self, request: Request, default_download_format="zip"):
+        self.input_request = deepcopy(request)
+        self.receipt = request.pop("receipt", True)
+        self.download_format = request.pop("download_format", default_download_format)
+        self.mapped_request = mapping.apply_mapping(request, self.mapping)  # type: ignore
+
+    def make_download_object(
+        self,
+        paths: Union[str, list],
+        **kwargs,
+    ):
+        from cads_adaptors.tools import download_tools
+
+        # Allow possibility of over-riding the download format from the adaptor
+        download_format = kwargs.get("download_format", self.download_format)
+
+        paths = ensure_list(paths)
+        filenames = [os.path.basename(path) for path in paths]
+        # TODO: use request-id instead of hash
+        kwargs.setdefault(
+            "base_target", f"{self.collection_id}-{hash(tuple(self.input_request))}"
+        )
+
+        # Allow adaptor possibility of over-riding request value
+        if kwargs.get("receipt", self.receipt):
+            receipt_kwargs = kwargs.pop("receipt_kwargs", {})
+            kwargs.setdefault(
+                "receipt", self.make_receipt(filenames=filenames, **receipt_kwargs)
+            )
+
+        return download_tools.DOWNLOAD_FORMATS[download_format](paths, **kwargs)
+
+    def make_receipt(
+        self,
+        input_request: Union[Request, None] = None,
+        download_size: Any = None,
+        filenames: list = [],
+        **kwargs,
+    ) -> dict[str, Any]:
+        """
+        Create a receipt to be included in the downloaded archive.
+
+        **kwargs contains any other fields that are calculated during the runtime of the adaptor
+        """
+        from datetime import datetime as dt
+
+        # Allow adaptor to override and provide sanitized "input_request" if necessary
+        if input_request is None:
+            input_request = self.input_request
+
+        # Update kwargs with default values
+        if download_size is None:
+            download_size = "unknown"
+
+        receipt = {
+            "collection-id": self.collection_id,
+            "request": input_request,
+            "request-timestamp": dt.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "download-size": download_size,
+            "filenames": filenames,
+            # Get static URLs:
+            "user-support": "https://support.ecmwf.int",
+            "privacy-policy": "https://cds.climate.copernicus.eu/disclaimer-privacy",
+            # TODO: Change to URLs for licence instead of slug
+            "licence": [
+                f"{licence[0]} (version {licence[1]})" for licence in self.licences
+            ],
+            # TODO: Add request-id information to the context
+            "request-uid": self.config.get("request_uid", "Unavailable"),
+            #
+            # TODO: Add URL/DNS information to the context for populating these fields:
+            # "web-portal": self.???, # Need update to information available to adaptors
+            # "api-access": "https://url-to-data-api/{self.collection_id}"
+            # "metadata-api-access": "https://url-to-metadata-api/{self.collection_id}"
+            #
+            # TODO: Add metadata information to config, this could also be done via the metadata api
+            # "citation": self.???, # Need update to information available to adaptors
+            **kwargs,
+            **self.config.get("additional_receipt_info", {}),
+        }
+
+        return receipt
--- cads_adaptors/tools/download_tools.py
+++ cads_adaptors/tools/download_tools.py
@@ -1,11 +1,15 @@
 import os
-from typing import BinaryIO, Callable, Dict, List
+from typing import Any, BinaryIO, Callable, Dict, List, Union
+
+import yaml
 
 from cads_adaptors.tools.general import ensure_list
 
 
 # TODO zipstream for archive creation
-def zip_paths(paths: List[str], base_target: str = "output-data", **kwargs) -> BinaryIO:
+def zip_paths(
+    paths: List[str], base_target: str = "output-data", receipt: Any = None, **kwargs
+) -> BinaryIO:
     import zipfile
 
     target = f"{base_target}.zip"
@@ -17,6 +21,13 @@ def zip_paths(paths: List[str], base_target: str = "output-data", **kwargs) -> B
                 archive_name = os.path.basename(path)
             archive.write(path, archive_name)
 
+        if receipt is not None:
+            yaml_output: str = yaml.safe_dump(receipt, indent=2)
+            archive.writestr(
+                f"receipt-{base_target}.yaml",
+                data=yaml_output,
+            )
+
     for path in paths:
         os.remove(path)
 
@@ -27,6 +38,7 @@ def zip_paths(paths: List[str], base_target: str = "output-data", **kwargs) -> B
 def targz_paths(
     paths: List[str],
     base_target: str = "output-data",
+    receipt: Any = None,
     **kwargs,
 ) -> BinaryIO:
     import tarfile
@@ -40,6 +52,12 @@ def targz_paths(
                 archive_name = os.path.basename(path)
             archive.add(path, arcname=archive_name)
 
+        if receipt is not None:
+            receipt_fname = f"receipt-{base_target}.yaml"
+            with open(receipt_fname, "w") as receipt_file:
+                yaml.safe_dump(receipt, stream=receipt_file, indent=2)
+            archive.add(receipt_fname)
+
     for path in paths:
         os.remove(path)
 
@@ -49,14 +67,21 @@ def targz_paths(
 def list_paths(
     paths: List[str],
     **kwargs,
-) -> List:
+) -> List[BinaryIO]:
+    if kwargs.get("receipt") is not None:
+        receipt_fname = f"receipt-{kwargs.get('base_target', 'nohash')}.yaml"
+        with open(receipt_fname, "w") as receipt_file:
+            yaml.safe_dump(kwargs.get("receipt"), stream=receipt_file, indent=2)
+        paths.append(receipt_fname)
     return [open(path, "rb") for path in ensure_list(paths)]
 
 
-def as_source(paths: List[str], **kwargs) -> BinaryIO:
+def as_source(paths: List[str], **kwargs) -> Union[BinaryIO, List[BinaryIO]]:
     # Only return as_source if a single path, otherwise list MUST be requested
-    assert len(paths) == 1
-    return open(paths[0], "rb")
+    if len(paths) == 1:
+        return open(paths[0], "rb")
+    else:
+        return list_paths(paths, **kwargs)
 
 
 DOWNLOAD_FORMATS: Dict[str, Callable] = {
