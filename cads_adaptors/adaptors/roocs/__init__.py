3c17d878e8f75b65ae8229345bb3cc957ab67ca5
James Varndell <36902610+JamesVarndell@users.noreply.github.com>
2024-03-01 14:55:43 +0000

    ROOCS debug

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -94,6 +94,4 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
             raise ValueError(f"No data found for request {request}")
 
         # raise ValueError(str(raw_candidate) + " | " + str(self.facets_order))
-        print("RESULT: ", raw_candidate)
-        return raw_candidate
-        # return {key: raw_candidate[key] for key in self.facets_order}
+        return {key: raw_candidate[key] for key in self.facets_order}
