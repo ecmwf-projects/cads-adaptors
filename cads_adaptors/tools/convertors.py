3ab459cc22b0ab421e00d9bf0d1cd4df8dcd8ae2
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-03-07 09:17:29 +0000

    back to cfgrib

--- cads_adaptors/tools/convertors.py
+++ cads_adaptors/tools/convertors.py
@@ -14,44 +14,43 @@ def grib_to_netcdf_files(
     fname, extension = os.path.splitext(os.path.basename(grib_file))
     grib_file = os.path.realpath(grib_file)
 
-    from earthkit import data as ek_data
+    import cfgrib
+    if open_datasets_kwargs is None:
+        open_datasets_kwargs = {
+            "chunks": {"time": 1}   # Auto chunking
+        }
+    datasets = cfgrib.open_datasets(grib_file, **open_datasets_kwargs)
 
-    dataset = ek_data.from_source("file", grib_file)
+    if compression_options == "default":
+        compression_options = DEFAULT_COMPRESSION_OPTIONS
 
-    # Assume full data cube:
-    out_file = os.path.join(os.path.dirname(grib_file), f"{fname}.nc")
+    if compression_options is not None:
+        to_netcdf_kwargs.setdefault(
+            "engine", compression_options.pop("engine", "h5netcdf")
+        )
 
-    dataset.to_netcdf(out_file)
+    out_nc_files = []
+    for i, dataset in enumerate(datasets):
+        if compression_options is not None:
+            to_netcdf_kwargs.update(
+                {
+                    "encoding": {var: compression_options for var in dataset},
+                }
+            )
+        out_fname = f"{fname}_{i}.nc"
+        dataset.to_netcdf(out_fname, **to_netcdf_kwargs)
+        out_nc_files.append(out_fname)
+
+    return out_nc_files
 
 
+    # from earthkit import data as ek_data
 
-    
+    # dataset = ek_data.from_source("file", grib_file)
 
-    # import cfgrib
-    # if open_datasets_kwargs is None:
-    #     open_datasets_kwargs = {
-    #         "chunks": {"time": 1}   # Auto chunking
-    #     }
-    # datasets = cfgrib.open_datasets(grib_file, **open_datasets_kwargs)
+    # # Assume full data cube:
+    # out_file = os.path.join(os.path.dirname(grib_file), f"{fname}.nc")
 
-    # if compression_options == "default":
-    #     compression_options = DEFAULT_COMPRESSION_OPTIONS
+    # dataset.to_netcdf(out_file)
 
-    # if compression_options is not None:
-    #     to_netcdf_kwargs.setdefault(
-    #         "engine", compression_options.pop("engine", "h5netcdf")
-    #     )
 
-    # out_nc_files = []
-    # for i, dataset in enumerate(datasets):
-    #     if compression_options is not None:
-    #         to_netcdf_kwargs.update(
-    #             {
-    #                 "encoding": {var: compression_options for var in dataset},
-    #             }
-    #         )
-    #     out_fname = f"{fname}_{i}.nc"
-    #     dataset.to_netcdf(out_fname, **to_netcdf_kwargs)
-    #     out_nc_files.append(out_fname)
-
-    # return out_nc_files
