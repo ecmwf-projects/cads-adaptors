27e01a8601f517e49cc6fadd633f331d7a2770d9
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2024-03-13 14:20:21 +0000

    Weighted costs (#93)
    * ensure selection is a set
    
    * fill selection with dummy values
    
    * fill selection with dummy values
    
    * fill selection with dummy values
    
    * thorough costing
    
    * More compatible than expected
    
    * costing
    
    * some more tesrt cases
    
    * some more tesrt cases
    
    * QA

--- cads_adaptors/costing.py
+++ cads_adaptors/costing.py
@@ -4,10 +4,22 @@ from typing import Any
 
 from . import constraints
 
-EXCLUDED_WIDGETS = ["GeographicExtentWidget", "UnknownWidget"]
+EXCLUDED_WIDGETS = [
+    "GeographicExtentWidget",
+    "UnknownWidget",
+]
 
 # TODO: Handle DateRangeWidget
 # , "DateRangeWidget"]
+
+
+def ensure_set(input_item):
+    if not isinstance(input_item, set):
+        if isinstance(input_item, (list, tuple)):
+            return set(input_item)
+        else:
+            return {input_item}
+    return input_item
 
 
 def compute_combinations(d: dict[str, set[str]]) -> list[dict[str, str]]:
@@ -27,36 +39,32 @@ def remove_duplicates(found: list[dict[str, set[str]]]) -> list[dict[str, str]]:
 
 def count_combinations(
     found: list[dict[str, set[str]]],
-    selected_but_always_valid: list[str] = [],
     weighted_keys: dict[str, int] = dict(),
     weighted_values: dict[str, dict[str, int]] = dict(),
 ) -> int:  # TODO: integer is not strictly required
     granules = remove_duplicates(found)
+
     if len(weighted_values) > 0:
-        n_granules = 0
+        w_granules = []  # Weight of each granule
         for granule in granules:
             w_granule = 1
             for key, w_values in weighted_values.items():
-                if key in granule or key in selected_but_always_valid:
+                if key in granule:
                     for value, weight in w_values.items():
                         if value == granule[key]:
                             w_granule *= weight
-            n_granules += w_granule
+            w_granules.append(w_granule)
     else:
-        n_granules = len(granules)
+        w_granules = [1 for _ in granules]
 
-    if n_granules:
-        for key, weight in weighted_keys.items():
-            values = [granule[key] for granule in granules if key in granule]
-            unique_values = set(values)
-            n_unique_values = len(unique_values)
-            # Factor to account for fact that not all granules will have the weighted keys
-            included_factor = len(values) / len(granules)
-            # Need to subtract 1 from weight as weight=1 is accounted for in initial estimate
-            # n_granules += n_granules * (int(n_unique_values * (weight-1) * included_factor)-1)
-            # Equivalent to above, but simplified:
-            n_granules *= int(n_unique_values * included_factor * (weight - 1))
+    for key, weight in weighted_keys.items():
+        w_granules = [
+            w_granule * weight
+            for w_granule, granule in zip(w_granules, granules)
+            if key in granule
+        ]
 
+    n_granules = sum(w_granules)
     return n_granules
 
 
@@ -70,47 +78,49 @@ def estimate_granules(
     ] = dict(),  # Mapping of widget key to values-weights
     safe: bool = True,
 ) -> int:
-    # Ensure contraints are sets
-    _constraints = [
-        {k: set(v) for k, v in constraint.items()} for constraint in _constraints
-    ]
-    constraint_keys = constraints.get_keys(_constraints)
-    always_valid = constraints.get_always_valid_params(form_key_values, constraint_keys)
-    selected_but_always_valid = {
-        k: v for k, v in selection.items() if k in always_valid
-    }
-    always_valid_multiplier = math.prod(map(len, selected_but_always_valid.values()))
-    selected_constrained = {
-        k: v for k, v in selection.items() if k not in always_valid.keys()
-    }
-    found = []
-    # Apply constraints prior to ensure real cost is calculated
-    for constraint in _constraints:
-        intersection = {}
-        ok = True
-        for key, values in constraint.items():
-            if key in selected_constrained.keys():
-                common = values.intersection(selected_constrained[key])
-                if common:
-                    intersection.update({key: common})
+    if len(_constraints) > 0:
+        # Ensure contraints are sets
+        _constraints = [
+            {k: set(v) for k, v in constraint.items()} for constraint in _constraints
+        ]
+        constraint_keys = constraints.get_keys(_constraints)
+        always_valid = constraints.get_always_valid_params(
+            form_key_values, constraint_keys
+        )
+        selected_but_always_valid = {
+            k: v for k, v in selection.items() if k in always_valid
+        }
+        selected_constrained = {
+            k: v for k, v in selection.items() if k not in always_valid.keys()
+        }
+        found = []
+        # Apply constraints prior to ensure real cost is calculated
+        for constraint in _constraints:
+            intersection = {}
+            ok = True
+            for key, values in constraint.items():
+                if key in selected_constrained.keys():
+                    common = values.intersection(selected_constrained[key])
+                    if common:
+                        intersection.update({key: common})
+                    else:
+                        ok = False
+                        break
                 else:
                     ok = False
                     break
-            else:
-                ok = False
-                break
-        if ok:
-            if intersection not in found:
-                found.append(intersection)
-    if safe:
-        n_granules = count_combinations(
-            found, list(selected_but_always_valid), weighted_keys, weighted_values
-        )
-        return (n_granules) * max(1, always_valid_multiplier)
+            if ok:
+                intersection.update(selected_but_always_valid)
+                if intersection not in found:
+                    found.append(intersection)
     else:
-        return sum([math.prod([len(e) for e in d.values()]) for d in found]) * max(
-            1, always_valid_multiplier
-        )
+        selected_but_always_valid = {}
+        found = [selection]
+    if safe:
+        n_granules = count_combinations(found, weighted_keys, weighted_values)
+        return n_granules
+    else:
+        return sum([math.prod([len(e) for e in d.values()]) for d in found])
 
 
 def estimate_size(
@@ -126,11 +136,15 @@ def estimate_size(
 ) -> int:
     ignore_keys += get_excluded_keys(form)
 
-    this_selection: dict[str, set[str]] = {
-        k: v for k, v in selection.items() if k not in ignore_keys
-    }
-
     form_key_values = constraints.parse_form(form)
+
+    # Build selection for calculating costs, any missing fields are filled with a DUMMY value,
+    #  This may be problematic for DateRangeWidget
+    this_selection: dict[str, set[str]] = {
+        widget: ensure_set(selection.get(widget, list(values)[0]))
+        for widget, values in form_key_values.items()
+        if widget not in ignore_keys
+    }
 
     return (
         estimate_granules(
