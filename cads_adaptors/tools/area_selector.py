0b9b2c20786cea9c2680058543df8580f54dc743
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-03-18 16:02:40 +0000

    compute area subselect

--- cads_adaptors/tools/area_selector.py
+++ cads_adaptors/tools/area_selector.py
@@ -202,7 +202,7 @@ def area_selector_paths(
                 path.split(".")[:-1] + ["area-subset"] + [str(a) for a in area] + ["nc"]
             )
             context.logger.debug(f"out_fname: {out_fname}")
-            ds_area.to_netcdf(out_fname)
+            ds_area.compute().to_netcdf(out_fname)
             out_paths.append(out_fname)
         else:
             raise NotImplementedError(f"Output format not recognised {out_format}")
