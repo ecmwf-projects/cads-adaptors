c2d6b74a3cb74604a4fc439d6d663eda772c28ff
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-03-20 08:19:13 +0000

    url auth

--- cads_adaptors/tools/url_tools.py
+++ cads_adaptors/tools/url_tools.py
@@ -33,6 +33,8 @@ def try_download(urls: List[str], context: Context, **kwargs) -> List[str]:
     for url in urls:
         path = urllib.parse.urlparse(url).path.lstrip("/")
         dir = os.path.dirname(path)
+        print(kwargs)
+        context.logger.info(kwargs)
         if dir:
             os.makedirs(dir, exist_ok=True)
         try:
