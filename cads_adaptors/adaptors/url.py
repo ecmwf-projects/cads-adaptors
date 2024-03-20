bb8141c75f68fb0da2ff0e3f059f6f6520dd1e34
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-03-20 08:33:24 +0000

    url auth

--- cads_adaptors/adaptors/url.py
+++ cads_adaptors/adaptors/url.py
@@ -24,7 +24,7 @@ class UrlCdsAdaptor(cds.AbstractCdsAdaptor):
         # try to download URLs
         urls = [ru["url"] for ru in requests_urls]
         download_kwargs: dict[str, Any] = self.config.get("download_kwargs", {})
-        # Handle legacy syntax for suthentication
+        # Handle legacy syntax for authentication
         if "auth" in self.config:
             download_kwargs.setdefault(
                 "auth", (self.config["auth"]["username"], self.config["auth"]["password"])
