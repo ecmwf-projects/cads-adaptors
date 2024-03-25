77105a8f34f107e2f4db2fa262d9e18d9ad69bfe
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2024-03-25 15:12:42 +0000

    77 authorisationcredentials used by url adaptor (#95)
    * url auth
    
    * url auth
    
    * multiurl min version
    
    * QA

--- cads_adaptors/adaptors/url.py
+++ cads_adaptors/adaptors/url.py
@@ -1,4 +1,4 @@
-from typing import BinaryIO
+from typing import Any, BinaryIO
 
 from cads_adaptors.adaptors import Request, cds
 
@@ -23,7 +23,14 @@ class UrlCdsAdaptor(cds.AbstractCdsAdaptor):
 
         # try to download URLs
         urls = [ru["url"] for ru in requests_urls]
-        paths = url_tools.try_download(urls, context=self.context)
+        download_kwargs: dict[str, Any] = self.config.get("download_kwargs", {})
+        # Handle legacy syntax for authentication
+        if "auth" in self.config:
+            download_kwargs.setdefault(
+                "auth",
+                (self.config["auth"]["username"], self.config["auth"]["password"]),
+            )
+        paths = url_tools.try_download(urls, context=self.context, **download_kwargs)
 
         if area is not None:
             paths = area_selector.area_selector_paths(paths, area, self.context)
