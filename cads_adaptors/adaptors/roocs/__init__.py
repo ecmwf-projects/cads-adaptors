acadda5bd91ad7e25398901f5153191ec5bf403a
James Varndell <james.varndell@ecmwf.int>
2024-04-02 16:35:52 +0100

    DEBUG

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -28,6 +28,8 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
         request = mapping.apply_mapping(request, self.mapping)
 
         workflow = self.construct_workflow(request)
+        
+        raise ValueError(workflow._serialise())
 
         response = workflow.orchestrate()
 
