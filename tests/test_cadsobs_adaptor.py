f49cd1f68e4633c6446dbea4287f2bcbac97298a
garciam <garciam@predictia.es>
2024-04-29 14:57:42 +0200

    use h5netcdf for the assert not xarray

--- tests/test_cadsobs_adaptor.py
+++ tests/test_cadsobs_adaptor.py
@@ -1,153 +1,8 @@
-import datetime
 from pathlib import Path
 
-import xarray
-from numpy import nan
+import h5netcdf
 
-0
-
-
-def mocked_retrieve_observations(
-    session, storage_url, retrieve_args, output_dir: Path, size_limit: int
-) -> Path:
-    dataset = xarray.Dataset.from_dict(
-        {
-            "coords": {},
-            "attrs": {
-                "featureType": "point",
-                "contactemail": "https://support.ecmwf.int",
-                "licence_list": "20180314_Copernicus_License_V1.1",
-                "responsible_organisation": "ECMWF",
-            },
-            "dims": {"index": 5},
-            "data_vars": {
-                "observation_id": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [101, 102, 103, 104, 105],
-                },
-                "report_id": {"dims": ("index",), "attrs": {}, "data": [2, 2, 2, 2, 2]},
-                "platform_type": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [b"STN", b"STN", b"STN", b"STN", b"STN"],
-                },
-                "primary_station_id": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [b"7", b"7", b"7", b"7", b"7"],
-                },
-                "height_of_station_above_sea_level": {
-                    "dims": ("index",),
-                    "attrs": {"units": "m"},
-                    "data": [283.0, 283.0, 283.0, 283.0, 283.0],
-                },
-                "report_timestamp": {
-                    "dims": ("index",),
-                    "attrs": {"standard_name": "time"},
-                    "data": [
-                        datetime.datetime(1969, 2, 13, 5, 45),
-                        datetime.datetime(1969, 2, 13, 5, 45),
-                        datetime.datetime(1969, 2, 13, 5, 45),
-                        datetime.datetime(1969, 2, 13, 5, 45),
-                        datetime.datetime(1969, 2, 13, 5, 45),
-                    ],
-                },
-                "z_coordinate": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [98510.0, 94160.0, 92500.0, 90000.0, 86070.0],
-                },
-                "z_coordinate_type": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [
-                        b"pressure (Pa)",
-                        b"pressure (Pa)",
-                        b"pressure (Pa)",
-                        b"pressure (Pa)",
-                        b"pressure (Pa)",
-                    ],
-                },
-                "observed_variable": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [
-                        b"air_temperature",
-                        b"air_temperature",
-                        b"air_temperature",
-                        b"air_temperature",
-                        b"air_temperature",
-                    ],
-                },
-                "observation_value": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [
-                        290.45001220703125,
-                        287.8500061035156,
-                        286.8500061035156,
-                        285.25,
-                        283.45001220703125,
-                    ],
-                },
-                "units": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [b"Kelvin", b"Kelvin", b"Kelvin", b"Kelvin", b"Kelvin"],
-                },
-                "sensor_id": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [b"KC-68", b"KC-68", b"KC-68", b"KC-68", b"KC-68"],
-                },
-                "longitude|observations_table": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [nan, nan, nan, nan, nan],
-                },
-                "latitude|observations_table": {
-                    "dims": ("index",),
-                    "attrs": {},
-                    "data": [nan, nan, nan, nan, nan],
-                },
-                "longitude": {
-                    "dims": ("index",),
-                    "attrs": {
-                        "cdm_table": "station_configuration",
-                        "standard_name": "longitude",
-                        "units": "degrees_east",
-                    },
-                    "data": [
-                        130.60000610351562,
-                        130.60000610351562,
-                        130.60000610351562,
-                        130.60000610351562,
-                        130.60000610351562,
-                    ],
-                },
-                "latitude": {
-                    "dims": ("index",),
-                    "attrs": {
-                        "cdm_table": "station_configuration",
-                        "standard_name": "latitude",
-                        "units": "degrees_north",
-                    },
-                    "data": [
-                        31.600000381469727,
-                        31.600000381469727,
-                        31.600000381469727,
-                        31.600000381469727,
-                        31.600000381469727,
-                    ],
-                },
-            },
-        }
-    )
-    output_file = Path(output_dir, "test_cadsobs_adaptor.nc")
-    dataset.to_netcdf(output_file)
-    return output_file
-
+from cads_adaptors import ObservationsAdaptor
 
 CDM_LITE_VARIABLES = [
     "observation_id",
@@ -227,10 +82,7 @@ class MockerCadsobsApiClient:
         ]
 
 
-# @pytest.mark.skip("Depends on cdsobs")
 def test_adaptor(tmp_path, monkeypatch):
-    from cads_adaptors import ObservationsAdaptor
-
     monkeypatch.setattr(
         "cads_adaptors.adaptors.cadsobs.adaptor.CadsobsApiClient",
         MockerCadsobsApiClient,
@@ -271,5 +123,6 @@ def test_adaptor(tmp_path, monkeypatch):
     with tempfile.open("wb") as tmpf:
         tmpf.write(result.read())
     assert tempfile.stat().st_size > 0
-    actual = xarray.open_dataset(tempfile)
-    assert actual.observation_id.size > 0
+    actual = h5netcdf.File(tempfile)
+    # TODO: Shouldn't be observation_id??
+    assert actual.dimensions["index"].size > 0
