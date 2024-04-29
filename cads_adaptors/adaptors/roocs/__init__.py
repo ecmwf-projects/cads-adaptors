8ee9edd076f46cc92071b320d952e3d310c86fdc
James Varndell <james.varndell@ecmwf.int>
2024-04-29 19:40:48 +0100

    Debugging CMIP6 decadal

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -131,11 +131,11 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
 
         for raw_candidate in self.facets:
             candidate = raw_candidate.copy()
-            tmp_request = request.copy()  
+            tmp_request = request.copy()
             regex_facets = {
                 key: self.facet_search[key].format(**{key: tmp_request.pop(key)})
                 for key in self.facet_search
-            }              
+            }
 
             for key, groups in self.facet_groups.items():
                 if key in candidate:
@@ -144,10 +144,12 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
                             candidate[key] = group
 
             if candidate.items() >= tmp_request.items():
-                # for key, value in regex_facets.items():
-                #     if not re.search(value, candidate[key]):
-                #         break
-                # else:
+                matched_facets.append(raw_candidate)
+            elif candidate.items() < tmp_request.items():
+                for key, value in regex_facets.items():
+                    if not re.search(value, candidate[key]):
+                        break
+                else:
                     matched_facets.append(candidate)
         
         if not matched_facets:
