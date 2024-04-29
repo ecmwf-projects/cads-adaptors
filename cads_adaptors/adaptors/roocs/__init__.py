3f5b81beac6f4d270473c3a24bffda1ae3f54bf1
James Varndell <james.varndell@ecmwf.int>
2024-04-29 15:58:16 +0100

    Debug regex facets

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -141,6 +141,12 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
             if candidate.items() >= tmp_request.items():
                 matched_facets.append(raw_candidate)
             elif candidate.items() < tmp_request.items():
+                print(
+                    f"--------------------------"
+                    f"CANDIDATE: {candidate}\n"
+                    f"REQUEST: {tmp_request}"
+                    f"--------------------------"
+                )
                 for key, value in regex_facets.items():
                     if not re.search(value, candidate[key]):
                         break
