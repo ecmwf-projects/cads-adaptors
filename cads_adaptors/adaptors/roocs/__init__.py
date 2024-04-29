447710ee337fdface7b94516d71f148536d3f9d7
James Varndell <james.varndell@ecmwf.int>
2024-04-29 19:06:42 +0100

    Debugging CMIP6 decadal

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -28,9 +28,6 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
         request = mapping.apply_mapping(request, self.mapping)
 
         workflow = self.construct_workflow(request)
-        
-        # raise ValueError(workflow._serialise())
-        
         response = rooki.rooki.orchestrate(workflow=workflow._serialise())
 
         response = workflow.orchestrate()
@@ -126,14 +123,14 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
         request = {k: v for k, v in request.items() if k in self.facets[0]}
 
         matched_facets = []
-        regex_facets = {
-            key: self.facet_search[key].format(**{key: request.pop(key)})
-            for key in self.facet_search
-        }
 
         for raw_candidate in self.facets:
             candidate = raw_candidate.copy()
-            tmp_request = request.copy()                
+            tmp_request = request.copy()
+            regex_facets = {
+                key: self.facet_search[key].format(**{key: tmp_request.pop(key)})
+                for key in self.facet_search
+            }
 
             for key, groups in self.facet_groups.items():
                 if key in candidate:
@@ -142,11 +139,13 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
                             candidate[key] = group
 
             if candidate.items() >= tmp_request.items():
-                # for key, value in regex_facets.items():
-                #     if not re.search(value, candidate[key]):
-                #         break
-                # else:
-                matched_facets.append(candidate)
+                matched_facets.append(raw_candidate)
+            elif candidate.items() < tmp_request.items():
+                for key, value in regex_facets.items():
+                    if not re.search(value, candidate[key]):
+                        break
+                else:
+                    matched_facets.append(candidate)
         
         if not matched_facets:
             raise ValueError(f"No data found for request {request}")
