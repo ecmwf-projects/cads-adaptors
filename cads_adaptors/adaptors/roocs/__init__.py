d0c1e80b22413d669c67f57f61007606231b33b6
James Varndell <james.varndell@ecmwf.int>
2024-04-30 11:36:03 +0100

    Debugging CMIP6 decadal

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -49,22 +49,22 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
         dataset_ids = [
             ".".join(facet for facet in sub_facets.values() if facet is not None)
             for sub_facets in facets
-        ]
+        ][0]
         variable_id = facets[0].get("variable", "")
 
         workflow = rookops.Input(variable_id, dataset_ids)
 
-        for operator, operator_kwargs in self.operators.items():
-            tmp_kwargs = operator_kwargs.copy()
-            for key, value in operator_kwargs.items():
-                if "." in value:
-                    klass, method = value.split(".")
-                    tmp_kwargs.pop(key)
-                    tmp_kwargs = {
-                        **tmp_kwargs,
-                        **getattr(getattr(operators, klass.capitalize())(request), method)()
-                    }
-            workflow = getattr(rookops, operator)(workflow, **tmp_kwargs)
+        # for operator, operator_kwargs in self.operators.items():
+        #     tmp_kwargs = operator_kwargs.copy()
+        #     for key, value in operator_kwargs.items():
+        #         if "." in value:
+        #             klass, method = value.split(".")
+        #             tmp_kwargs.pop(key)
+        #             tmp_kwargs = {
+        #                 **tmp_kwargs,
+        #                 **getattr(getattr(operators, klass.capitalize())(request), method)()
+        #             }
+        #     workflow = getattr(rookops, operator)(workflow, **tmp_kwargs)
 
         for operator_class in operators.ROOKI_OPERATORS:
             operator = operator_class(request)
