82596582e7eac0a3d510e8fa61763d3dc3642fcb
James Varndell <james.varndell@ecmwf.int>
2024-05-01 18:44:29 +0100

    Debugging CMIP6 decadal

--- cads_adaptors/adaptors/roocs/__init__.py
+++ cads_adaptors/adaptors/roocs/__init__.py
@@ -22,20 +22,12 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
         os.environ["ROOK_URL"] = "http://rook.dkrz.de/wps"
 
         # switch off interactive logging to avoid threading issues
-        # os.environ["ROOK_MODE"] = "sync"
+        os.environ["ROOK_MODE"] = self.config.get("ROOK_MODE", "sync")
 
         request = mapping.apply_mapping(request, self.mapping)
 
         workflow = self.construct_workflow(request)
         response = workflow.orchestrate()
-        
-        # thread = threading.Thread(
-        #     target=self.run_request,
-        #     args=(workflow,),
-        # )
-        # thread.start()
-        # thread.join()
-        # response = workflow.orchestrate()
 
         try:
             urls = response.download_urls()
@@ -157,17 +149,3 @@ class RoocsCdsAdaptor(AbstractCdsAdaptor):
             {key: final_candidate[key] for key in self.facets_order}
             for final_candidate in matched_facets
         ]
-
-    def run_request(self, workflow):
-        from cads_adaptors.tools import download_tools, url_tools
-        response = workflow.orchestrate()
-
-        try:
-            urls = response.download_urls()
-        except Exception:
-            raise Exception(response.status)
-        urls += [response.provenance(), response.provenance_image()]
-
-        paths = url_tools.try_download(urls, context=self.context)
-
-        return download_tools.DOWNLOAD_FORMATS["zip"](paths)
