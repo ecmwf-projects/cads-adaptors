c41fb6773350920ffc840cea3d9b15148012bfdb
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2024-05-03 08:15:46 +0100

    username add to MARS request (#116)
    * username
    
    * username mypy

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -74,6 +74,7 @@ def execute_mars(
         ),
         "host": os.getenv("HOSTNAME"),
     }
+    env["username"] = str(env["namespace"]) + ":" + str(env["user_id"]).split("-")[-1]
 
     reply = cluster.execute(requests, env, target)
     if reply.error:
