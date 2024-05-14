4d5af00dfa060f4a31f4a40868023a59c344971a
Marco Cucchi <m.cucchi@bopen.eu>
2024-05-14 18:42:52 +0200

    simplify exceptions

--- cads_adaptors/exceptions.py
+++ cads_adaptors/exceptions.py
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import attrs
 
-
-@attrs.define
 class InvalidRequest(Exception):
     """Raised when an invalid request is sent to the adaptor."""
-
-    message: str = "Invalid request"
