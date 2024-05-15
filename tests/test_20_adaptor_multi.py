a7a154fca0e1ac9bd46ff450878920065e7ee240
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-05-15 09:28:22 +0100

    dont_split_tests

--- tests/test_20_adaptor_multi.py
+++ tests/test_20_adaptor_multi.py
@@ -21,6 +21,7 @@ ADAPTOR_CONFIG = {
                 "stat": ["mean"],
             },
             "required_keys": ["level"],
+            "dont_split_keys": ["dont_split"],
         },
         "max": {
             "entry_point": "cads_adaptors:DummyAdaptor",
@@ -69,6 +70,28 @@ def test_multi_adaptor_split_requests_required_keys():
     assert split_max_required_present == ADAPTOR_CONFIG["adaptors"]["max"]["values"]
 
 
+def test_multi_adaptor_split_requests_dont_split_keys():
+    multi_adaptor = multi.MultiAdaptor(FORM, **ADAPTOR_CONFIG)
+
+    request = REQUEST.copy()
+    request["dont_split"] = [1,2,3,4]
+    split_mean_dont_split_area = multi_adaptor.split_request(
+        request,
+        multi_adaptor.config["adaptors"]["mean"]["values"],
+        dont_split_keys=["dont_split"],
+    )
+    assert "dont_split" in split_mean_dont_split_area
+
+    # Area is dont_split as default
+    request["area"] = [1,2,3,4]
+    split_max_split_area = multi_adaptor.split_request(
+        request,
+        multi_adaptor.config["adaptors"]["max"]["values"],
+    )
+    assert "dont_split" not in split_max_split_area
+    assert "area" in split_max_split_area
+
+
 def test_multi_adaptor_split_adaptors():
     multi_adaptor = multi.MultiAdaptor(FORM, **ADAPTOR_CONFIG)
 
@@ -103,3 +126,19 @@ def test_multi_adaptor_split_adaptors_required_keys():
 
     assert "mean" not in sub_adaptors.keys()
     assert "max" in sub_adaptors.keys()
+
+
+def test_multi_adaptor_split_adaptors_dont_split_keys():
+    multi_adaptor = multi.MultiAdaptor(FORM, **ADAPTOR_CONFIG)
+
+    request = REQUEST.copy()
+    request["dont_split"] = [1,2,3,4]
+    # Area is dont_split as default
+    request["area"] = [1,2,3,4]
+    sub_adaptors = multi_adaptor.split_adaptors(
+        request,
+    )
+
+    assert "dont_split" in sub_adaptors["mean"][1].keys()
+    assert "dont_split" not in sub_adaptors["max"][1].keys()
+    assert "area" in sub_adaptors["max"][1].keys()
