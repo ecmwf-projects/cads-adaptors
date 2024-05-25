d47a5e40aaed53f39b9e02a2e0f9ad7ed1b67146
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2024-05-25 16:11:42 +0100

    CEAQFA

---
+++ cads_adaptors/adaptors/cams_regional_fc/__init__.py
@@ -0,0 +1,25 @@
+from cads_adaptors.adaptors.cds import AbstractCdsAdaptor
+from cads_adaptors.adaptors import Request
+from typing import Any, BinaryIO
+import logging
+import json
+
+import time
+import zipfile
+import logging
+from copy import deepcopy
+from datetime import datetime, timedelta
+
+# from cds_common.cams.regional_fc_api import regional_fc_api
+# from cds_common.system import cds_forms_dir
+# from cds_common import hcube_tools, tree_tools, date_tools
+
+class CAMSEuropeAirQualityForecastsAdaptor(AbstractCdsAdaptor):
+    def retrieve(self, request: Request) -> BinaryIO:
+        with open("dummy.grib", "w") as fp:
+            fp.write("From inside cads_adaptors:\n\n\n")
+            fp.write(json.dumps(self.config))
+            fp.write("\n\n\n")
+            fp.write(json.dumps(request))
+            
+        return open("dummy.grib", "rb")
