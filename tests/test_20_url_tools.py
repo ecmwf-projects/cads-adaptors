dde855cef9ab466951fffeb67b65eb3f8f8bc38d
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-05-30 10:46:38 +0100

    first draft of test for URL

--- tests/test_20_url_tools.py
+++ tests/test_20_url_tools.py
@@ -59,3 +59,83 @@ def test_ftp_download(tmp_path, ftpserver, anon):
     ]
     with open(local_test_file) as original, open(local_test_download) as downloaded:
         assert original.read() == downloaded.read()
+
+
+
+
+ADAPTOR_CONFIG = {
+    "collection_id": "test-adaptor-url-debugging",
+    "entry_point": "cads_adaptors:UrlCdsAdaptor",
+    "mapping": {
+        "force": {
+            "spatial_aggregation": [
+                "NUT0"
+            ]
+        },
+        "remap": {
+            "energy_product_type": {
+                "capacity_factor_ratio": "CFR",
+                "energy": "NRG",
+                "power": "PWR"
+            },
+            "experiment": {
+                "rcp_4_5": "45",
+                "rcp_8_5": "85"
+            },
+            "gcm": {
+                "ec_earth": "IC",
+                "hadgem2_es": "MO",
+                "mpi_esm_lr": "MP"
+            },
+            "rcm": {
+                "cclm4_8_17": "CC",
+                "racmo22e": "RA"
+            },
+            "temporal_aggregation": {
+                "monthly": "01m",
+                "seasonal": "03m"
+            },
+            "variable": {
+                "2m_air_temperature": "TA",
+                "electricity_demand": "EDM",
+                "surface_downwelling_shortwave_radiation": "GHI",
+                "wind_power_generation_onshore": "WON",
+                "wind_speed_at_10m": "10WS"
+            }
+        }
+    },
+    "patterns": [
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if experiment == '85' and variable == 'GHI' and gcm == 'MO' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAMO/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_GHI_0000m_Euro_{{ spatial_aggregation }}_S195101010130_E209812312230_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if variable == '10WS' and gcm == 'IC' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAIC/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_WS-_0010m_Euro_{{ spatial_aggregation }}_S195101010000_E210012312100_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if variable == '10WS' and gcm == 'MO' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAMO/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_WS-_0010m_Euro_{{ spatial_aggregation }}_S195101010000_E209812312100_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if variable == 'GHI' and gcm == 'IC' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAIC/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_GHI_0000m_Euro_{{ spatial_aggregation }}_S195101010130_E210012312230_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if variable == 'TA' and gcm == 'IC' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAIC/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_TA-_0002m_Euro_{{ spatial_aggregation }}_S195101010000_E210012312100_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if (energy_product_type is not defined or energy_product_type is none) %}{% if variable == 'TA' and gcm == 'MO' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/CLIM/RAMO/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_TA-_0002m_Euro_{{ spatial_aggregation }}_S195101010000_E209812312100_INS_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_NA---.csv{% endif %}{% endif %}",
+        "{% if experiment == '45' and variable == 'EDM' and rcm == 'CC' and gcm == 'MP' and energy_product_type in ['NRG','PWR'] %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/ENER/CCMP/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_EDM_0000m_Euro_{{ spatial_aggregation }}_S197001010000_E210012310000_{{ energy_product_type }}_TIM_{{ temporal_aggregation }}_NA-_noc_org_01_RCP{{ experiment }}_NA---_GamNT.csv{% endif %}",
+        "{% if variable == 'EDM' and rcm == 'RA' and gcm == 'IC' and energy_product_type in ['NRG','PWR'] %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/ENER/RAIC/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_EDM_0000m_Euro_{{ spatial_aggregation }}_S197001010000_E210012310000_{{ energy_product_type }}_TIM_{{ temporal_aggregation }}_NA-_noc_org_01_RCP{{ experiment }}_NA---_GamNT.csv{% endif %}",
+        "{% if variable == 'EDM' and rcm == 'RA' and gcm == 'MO' and energy_product_type in ['NRG','PWR'] %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/ENER/RAMO/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_EDM_0000m_Euro_{{ spatial_aggregation }}_S197001010000_E209812310000_{{ energy_product_type }}_TIM_{{ temporal_aggregation }}_NA-_noc_org_01_RCP{{ experiment }}_NA---_GamNT.csv{% endif %}",
+        "{% if variable == 'WON' and rcm == 'RA' and gcm == 'IC' and energy_product_type == 'CFR' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/ENER/RAIC/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_WON_0100m_Euro_{{ spatial_aggregation }}_S195101010000_E210012312100_{{ energy_product_type }}_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_PhM01.csv{% endif %}",
+        "{% if variable == 'WON' and rcm == 'RA' and gcm == 'MO' and energy_product_type == 'CFR' %}http://energy-tools.copernicus-climate.eu/C3S_ENERGY/PROJ/ENER/RAMO/RCP{{ experiment }}/{{ variable }}/{{ spatial_aggregation }}/P_CMI5_{{ rcm }}{{ gcm }}_CM20_WON_0100m_Euro_{{ spatial_aggregation }}_S195101010000_E209812312100_{{ energy_product_type }}_TIM_{{ temporal_aggregation }}_NA-_cdf_org_01_RCP{{ experiment }}_NA---_PhM01.csv{% endif %}"
+    ]
+}
+
+CONSTRAINTS = [
+    {"experiment": ["rcp_4_5"], "gcm": ["ec_earth"], "rcm": ["racmo22e"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["wind_speed_at_10m", "surface_downwelling_shortwave_radiation", "2m_air_temperature"]},
+    {"experiment": ["rcp_4_5"], "gcm": ["hadgem2_es"], "rcm": ["racmo22e"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["wind_speed_at_10m", "2m_air_temperature"]},
+    {"experiment": ["rcp_8_5"], "gcm": ["ec_earth", "hadgem2_es"], "rcm": ["racmo22e"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["wind_speed_at_10m", "surface_downwelling_shortwave_radiation", "2m_air_temperature"]},
+    {"energy_product_type": ["capacity_factor_ratio"], "experiment": ["rcp_4_5", "rcp_8_5"], "gcm": ["ec_earth", "hadgem2_es"], "rcm": ["racmo22e"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["wind_power_generation_onshore"]},
+    {"energy_product_type": ["energy", "power"], "experiment": ["rcp_4_5"], "gcm": ["mpi_esm_lr"], "rcm": ["cclm4_8_17"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["electricity_demand"]},
+    {"energy_product_type": ["energy", "power"], "experiment": ["rcp_4_5", "rcp_8_5"], "gcm": ["ec_earth", "hadgem2_es"], "rcm": ["racmo22e"], "temporal_aggregation": ["monthly", "seasonal"], "variable": ["electricity_demand"]}
+]
+
+def test_find_all_urls():
+    request = {
+        "variable": ["surface_downwelling_shortwave_radiation", "electricity_demand"],
+        "spatial_aggregation": ["country_level"],
+        "temporal_aggregation": ["monthly"],
+        "energy_product_type": ["energy"],
+        "experiment": ["rcp_4_5", "rcp_8_5"],
+        "rcm": ["racmo22e", "cclm4_8_17"],
+        "gcm": ["hadgem2_es", "mpi_esm_lr"],
+    }
+
