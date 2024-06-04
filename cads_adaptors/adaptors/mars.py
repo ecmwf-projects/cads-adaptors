5adf399975134ddaa83b418d2ee0b75600d22d32
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-06-04 14:58:10 +0100

    correctly handle data_format

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -6,15 +6,27 @@ from cads_adaptors.tools.date_tools import implement_embargo
 from cads_adaptors.tools.general import ensure_list
 
 
+def handle_data_format(data_format: str) -> str:
+    if isinstance(data_format, (list, tuple, set)):
+        data_format = list(data_format)
+        assert len(data_format) == 1, "Only one value of data_format is allowed"
+        data_format = data_format[0]
+
+    if data_format in ["netcdf4", "netcdf", "nc"]:
+        data_format = "netcdf"
+    elif data_format in ["grib", "grib2", "grb", "grb2"]:
+        data_format = "grib"
+    else:
+        data_format = "grib"
+    return data_format
+
 def convert_format(
     result: str,
     data_format: str,
     context: Context,
     **kwargs,
 ) -> list:
-    if isinstance(data_format, (list, tuple)):
-        assert len(data_format) == 1, "Only one value of data_format is allowed"
-        data_format = data_format[0]
+    data_format = handle_data_format(data_format)
 
     if data_format in ["netcdf4", "netcdf", "nc"]:
         to_netcdf_kwargs: dict[str, Any] = {}
@@ -132,6 +144,7 @@ class MarsCdsAdaptor(cds.AbstractCdsAdaptor):
         # TODO: Remove legacy syntax all together
         data_format = request.pop("format", "grib")
         data_format = request.pop("data_format", data_format)
+        data_format = handle_data_format(data_format)
 
         # Account from some horribleness from teh legacy system:
         if data_format.lower() in ["netcdf.zip", "netcdf_zip", "netcdf4.zip"]:
