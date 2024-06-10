fe6dc83cc079138d39054484aa48db39b704b9b2
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2024-06-10 09:50:27 +0100

    Update tests/test_10_constraints.py
    Co-authored-by: Mattia Almansi <m.almansi@bopen.eu>

--- tests/test_10_constraints.py
+++ tests/test_10_constraints.py
@@ -383,14 +383,6 @@ def test_legacy_intersect_constraints():
 
 def test_legacy_intersect_empty_constraints():
     raw_constraints = []
-    request = {
-        "variable": ["surface_downwelling_shortwave_radiation", "electricity_demand"],
-        "spatial_aggregation": "country_level",
-        "temporal_aggregation": ["monthly"],
-        "energy_product_type": ["energy"],
-        "experiment": ["rcp_8_5"],
-        "rcm": ["racmo22e"],
-        "gcm": ["hadgem2_es"],
-    }
+    request = {"foo": "bar"}
     actual = constraints.legacy_intersect_constraints(request, raw_constraints)
     assert actual == [request]
