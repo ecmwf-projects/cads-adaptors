513d3c93744f35fd7b4fcb7b48512161ad10fff9
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-06-12 14:35:20 +0100

    Simple version of normalise request

--- tests/test_14_abstract_cds.py
+++ tests/test_14_abstract_cds.py
@@ -6,7 +6,14 @@ from cads_adaptors import DummyCdsAdaptor
 @pytest.mark.parametrize(
     "in_value",
     (
-        1, "1", [1], (1,), {1}, ["1"], {"1"}, ("1",),
+        1,
+        "1",
+        [1],
+        (1,),
+        {1},
+        ["1"],
+        {"1"},
+        ("1",),
     ),
 )
 def test_normalize_request(in_value):
@@ -17,19 +24,19 @@ def test_normalize_request(in_value):
 
 def test_normalize_request_complex():
     this_adaptor = DummyCdsAdaptor({}, collection_id="test-collection")
-    request = this_adaptor.normalise_request({
-        "test": 1,
-        "test2": "1",
-        "test3": [1, "2"],
-        "test4": (1, "2"),
-        "test5": {1, "2"}
-    })
+    request = this_adaptor.normalise_request(
+        {
+            "test": 1,
+            "test2": "1",
+            "test3": [1, "2"],
+            "test4": (1, "2"),
+            "test5": {1, "2"},
+        }
+    )
     assert request == {
         "test": ["1"],
         "test2": ["1"],
         "test3": ["1", "2"],
         "test4": ["1", "2"],
-        "test5": ["1", "2"]
+        "test5": ["1", "2"],
     }
-
-    
