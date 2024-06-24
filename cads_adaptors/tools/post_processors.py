eef586c9c6d7310ae0cb834a88b7d79677d1b9cf
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-06-24 13:45:17 +0100

    fine tuned

--- cads_adaptors/tools/post_processors.py
+++ cads_adaptors/tools/post_processors.py
@@ -4,10 +4,6 @@ from xarray import Dataset
 from cads_adaptors import Context
 
 CONFIG_MAPPING = {
-    "daily_statistics": {
-        "method": "temporal_reduction",
-        "frequency": "day",
-    },
     "daily_mean": {
         "method": "daily_statistics",
         "how": "mean",
@@ -24,10 +20,6 @@ CONFIG_MAPPING = {
         "method": "daily_statistics",
         "how": "max",
     },
-    "monthly_statistics": {
-        "method": "temporal_reduction",
-        "frequency": "month",
-    },
     "monthly_median": {
         "method": "monthly_statistics",
         "how": "median",
@@ -43,28 +35,51 @@ CONFIG_MAPPING = {
 }
 
 def pp_config_mapping(pp_config: dict[str, Any]) -> dict[str, Any]:
+    print(type(pp_config), pp_config)
     cnt = 0 # Escape infinite loop
     while pp_config.get("method") in CONFIG_MAPPING and cnt<100:
+        print(pp_config)
         pp_config = {**pp_config, **CONFIG_MAPPING[pp_config["method"]]}
     return pp_config
 
-def temporal_reduction(
+def daily_statistics(
     in_xarray_dict: dict[str, Dataset],
     context: Context = Context(),
     how: str | Callable = "mean",
-    frequency: str = "day",
     **kwargs,
 ) -> dict[str, Dataset]:
     from earthkit.aggregate import temporal
 
     out_xarray_dict = {}
     for in_tag, in_dataset in in_xarray_dict.items():
-        out_tag = f"{in_tag}_{how}_{frequency}"
-        context.add_stdout(f"Temporal reduction: {out_tag}")
-        out_xarray_dict[out_tag] = temporal(
+        out_tag = f"{in_tag}_daily-{how}"
+        context.add_stdout(f"Daily reduction: {how} {kwargs}")
+        context.add_user_visible_log(f"Temporal reduction: {how} {kwargs}")
+        out_xarray_dict[out_tag] = temporal.daily_reduce(
             in_dataset,
             how=how,
-            frequency=frequency,
+            **kwargs,
+        )
+
+    return out_xarray_dict
+
+
+def monthly_statistics(
+    in_xarray_dict: dict[str, Dataset],
+    context: Context = Context(),
+    how: str | Callable = "mean",
+    **kwargs,
+) -> dict[str, Dataset]:
+    from earthkit.aggregate import temporal
+
+    out_xarray_dict = {}
+    for in_tag, in_dataset in in_xarray_dict.items():
+        out_tag = f"{in_tag}_monthly-{how}"
+        context.add_stdout(f"Temporal reduction: {how} {kwargs}")
+        context.add_user_visible_log(f"Temporal reduction: {how} {kwargs}")
+        out_xarray_dict[out_tag] = temporal.monthly_reduce(
+            in_dataset,
+            how=how,
             **kwargs,
         )
 
