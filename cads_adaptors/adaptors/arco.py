14bfa2d39088c745ba3309c59efae8b40854cf22
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-06-25 17:45:45 +0100

    QA

--- cads_adaptors/adaptors/arco.py
+++ cads_adaptors/adaptors/arco.py
@@ -1,27 +1,31 @@
-from typing import Any, BinaryIO
 import os
+from typing import BinaryIO
 
 from cads_adaptors.adaptors import Request, cds
 from cads_adaptors.tools.general import ensure_list
 
+DEFAULT_BASE_URL = (
+    "https://s3.cds.ecmwf.int/swift/v1/AUTH_cf0252abd9434b5e859bc07c159ec2f4"
+)
 
-DEFAULT_BASE_URL = "https://s3.cds.ecmwf.int/swift/v1/AUTH_cf0252abd9434b5e859bc07c159ec2f4"
 
 class ArcoCdsAdaptor(cds.AbstractCdsAdaptor):
     def retrieve(self, request: Request) -> BinaryIO:
         import xarray as xr
 
         self._pre_retrieve(request=request)
-        BASEURL = self.config.get('BASEURL', DEFAULT_BASE_URL)
-        BUCKET = self.config.get('BUCKET', 'cadl-arco-geo/arco/era5/sfc/geoChunked.zarr')
+        BASEURL = self.config.get("BASEURL", DEFAULT_BASE_URL)
+        BUCKET = self.config.get(
+            "BUCKET", "cadl-arco-geo/arco/era5/sfc/geoChunked.zarr"
+        )
 
-        arco_ds = xr.open_zarr(os.join(BASEURL, BUCKET))
+        arco_ds = xr.open_zarr(os.path.join(BASEURL, BUCKET))
 
         variables = ensure_list(self.mapped_request.get("variable", []))
 
         arco_ds = arco_ds[variables]
         self.context.add_stdout(f"{self.mapped_request}")
-        
+
         location = self.mapped_request.get("location", {"latitude": 0, "longitude": 0})
         latitude = location["latitude"]
         longitude = location["longitude"]
@@ -36,4 +40,3 @@ class ArcoCdsAdaptor(cds.AbstractCdsAdaptor):
         arco_ds.to_netcdf("output.nc")
 
         return self.make_download_object("output.nc")
-
