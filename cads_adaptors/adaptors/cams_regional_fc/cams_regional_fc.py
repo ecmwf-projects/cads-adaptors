6f57f33790bb1380abede1575efbf318a875e9fb
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2024-07-09 16:40:10 +0100

    Debug

--- cads_adaptors/adaptors/cams_regional_fc/cams_regional_fc.py
+++ cads_adaptors/adaptors/cams_regional_fc/cams_regional_fc.py
@@ -175,7 +175,7 @@ def new_cams_regional_fc(context, config, requests, forms_dir=None):
     def create_result_file(self, extension):
         class MockResultFile():
             def __init__(self):
-                self.path = 'alabala.txt'
+                self.path = '/cache/debug/alabala.txt'
         self.add_stdout("----------> MOCK RESULT FILE HERE")
         return MockResultFile()
     
