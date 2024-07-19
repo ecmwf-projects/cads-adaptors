09893457d2c6b87a60c83075c04ac733a0786def
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-07-19 15:57:56 +0100

    split_on functionality

--- cads_adaptors/tools/general.py
+++ cads_adaptors/tools/general.py
@@ -8,3 +8,27 @@ def ensure_list(input_item: Any) -> list:
     if input_item is None:
         return []
     return [input_item]
+
+
+def split_requests_on_keys(
+    requests: list[dict[str, Any]], split_on_keys: list[str]
+) -> list[dict]:
+    """Split a request on keys, returning a list of requests."""
+    if len(split_on_keys) == 0:
+        return requests
+
+    out_requests = []
+    for request in requests:
+        for key in split_on_keys:
+            if key in request:
+                values = ensure_list(request[key])
+                if len(values) == 1:
+                    out_requests.append(request)
+                else:
+                    for value in values:
+                        new_request = request.copy()
+                        new_request[key] = value
+                        out_requests.append(new_request)
+            else:
+                out_requests.append(request)
+    return out_requests
--- tests/test_10_general_tools.py
+++ tests/test_10_general_tools.py
@@ -22,3 +22,51 @@ from cads_adaptors.tools import general
 )
 def test_general_ensure_list(input_item, expected_output):
     assert general.ensure_list(input_item) == expected_output
+
+
+# Test for split_requests_on_keys
+@pytest.mark.parametrize(
+    "requests, split_on_keys, expected_output",
+    [
+        (
+            [
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "value3", "key2": "value4"},
+            ],
+            ["key1"],
+            [
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "value3", "key2": "value4"},
+            ],
+        ),
+        (
+            [
+                {"key1": ["value1", "value2"], "key2": "value3"},
+                {"key1": "value4", "key2": "value5"},
+            ],
+            ["key1"],
+            [
+                {"key1": "value1", "key2": "value3"},
+                {"key1": "value2", "key2": "value3"},
+                {"key1": "value4", "key2": "value5"},
+            ],
+        ),
+        (
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+            ["key1"],
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+        ),
+        (
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+            ["key3"],
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+        ),
+        (
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+            [],
+            [{"key1": "value1", "key2": "value2"}, {"key2": "value3"}],
+        ),
+    ],
+)
+def test_general_split_requests_on_keys(requests, split_on_keys, expected_output):
+    assert general.split_requests_on_keys(requests, split_on_keys) == expected_output
