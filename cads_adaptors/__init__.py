98fb4bef160e43624bffea68b3b82af713b32278
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2024-08-05 15:04:54 +0100

    Steps towards a first rate limiting solution

--- cads_adaptors/__init__.py
+++ cads_adaptors/__init__.py
@@ -34,8 +34,7 @@ from cads_adaptors.adaptors.mars import DirectMarsCdsAdaptor, MarsCdsAdaptor
 from cads_adaptors.adaptors.multi import MultiAdaptor, MultiMarsCdsAdaptor
 from cads_adaptors.adaptors.roocs import RoocsCdsAdaptor
 from cads_adaptors.adaptors.url import UrlCdsAdaptor
-from cads_adaptors.adaptors.cams_regional_fc import CAMSEuropeAirQualityForecastsAdaptor
-#, CAMSEuropeAirQualityForecastsAdaptorForLatestData, CAMSEuropeAirQualityForecastsAdaptorForArchivedData
+from cads_adaptors.adaptors.cams_regional_fc import CAMSEuropeAirQualityForecastsAdaptor, CAMSEuropeAirQualityForecastsAdaptorForLatestData, CAMSEuropeAirQualityForecastsAdaptorForArchivedData
 
 from .tools.adaptor_tools import get_adaptor_class
 
@@ -57,7 +56,7 @@ __all__ = [
     "MultiMarsCdsAdaptor",
     "RoocsCdsAdaptor",
     "CAMSEuropeAirQualityForecastsAdaptor",
-    # "CAMSEuropeAirQualityForecastsAdaptorForLatestData",
-    # "CAMSEuropeAirQualityForecastsAdaptorForArchivedData",
+    "CAMSEuropeAirQualityForecastsAdaptorForLatestData",
+    "CAMSEuropeAirQualityForecastsAdaptorForArchivedData",
     "ObservationsAdaptor",
 ]
--- cads_adaptors/adaptors/cams_regional_fc/__init__.py
+++ cads_adaptors/adaptors/cams_regional_fc/__init__.py
@@ -23,27 +23,32 @@ from .api_retrieve import api_retrieve
 # - ARCHIVE: OK
 # - RATE LIMITING: TBD
 # - CACHE: TBD
+
+def debug_input(adaptor, request, message, output_file):
+    # dumping the config and request
+    with open(output_file, "w") as fp:
+        fp.write("\n\n\n")
+        fp.write(f'{message}')
+        fp.write(json.dumps(adaptor.config))
+        fp.write("\n\n\n")
+        fp.write(json.dumps(request))
+    return output_file
+    
+
 class CAMSEuropeAirQualityForecastsAdaptor(AbstractCdsAdaptor):
     def retrieve(self, request: Request) -> BinaryIO:
         request["type"] = [t.upper() for t in request["type"]]
         request["format"] = ['grib']
         result_file = new_cams_regional_fc(self.context, self.config, [request])
-
-        # # dumping the config and request
-        # with open("dummy.grib", "w") as fp:
-        #     fp.write("From inside cads_adaptors:\n\n\n")
-        #     fp.write(json.dumps(self.config))
-        #     fp.write("\n\n\n")
-        #     fp.write(json.dumps(request))
-        #     fp.write("\n\n\n")
-        #     fp.write(json.dumps(result_file))
             
         return open(result_file.path, "rb")
 
-# class CAMSEuropeAirQualityForecastsAdaptorForLatestData(AbstractCdsAdaptor):
-#     def retrieve(self, request: Request) -> BinaryIO:
-#         return None
+class CAMSEuropeAirQualityForecastsAdaptorForLatestData(AbstractCdsAdaptor):
+    def retrieve(self, request: Request) -> BinaryIO:
+        result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForLatestData:")
+        return open(result_file_path, "rb")
     
-# class CAMSEuropeAirQualityForecastsAdaptorForArchivedData(AbstractCdsAdaptor):
-#     def retrieve(self, request: Request) -> BinaryIO:
-#         return None
+class CAMSEuropeAirQualityForecastsAdaptorForArchivedData(AbstractCdsAdaptor):
+    def retrieve(self, request: Request) -> BinaryIO:
+        result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForArchivedData:")
+        return open(result_file_path, "rb")
