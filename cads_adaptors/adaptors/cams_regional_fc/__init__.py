d13b3b1dcbf837c7e89f444963e7f966a6c7b981
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2024-08-15 15:25:52 +0100

    Call retrieve_* from CAMSEuropeAirQualityForecastsAdaptorFor*Data

--- cads_adaptors/adaptors/cams_regional_fc/__init__.py
+++ cads_adaptors/adaptors/cams_regional_fc/__init__.py
@@ -14,7 +14,7 @@ import traceback
 # from cdscompute.decorators import configure
 from cdscompute.errors import NoDataException
 from cds_common.cams.regional_fc_api import regional_fc_api
-from .cams_regional_fc import cams_regional_fc,new_cams_regional_fc
+from .cams_regional_fc import cams_regional_fc,new_cams_regional_fc,retrieve_latest,retrieve_archived
 from .api_retrieve import api_retrieve
 
 
@@ -45,10 +45,12 @@ class CAMSEuropeAirQualityForecastsAdaptor(AbstractCdsAdaptor):
 
 class CAMSEuropeAirQualityForecastsAdaptorForLatestData(AbstractCdsAdaptor):
     def retrieve(self, request: Request) -> BinaryIO:
-        result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForLatestData:", 'dummy.txt')
-        return open(result_file_path, "rb")
+        #result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForLatestData:", 'dummy.txt')
+        result_file = retrieve_latest(self.context, request['requests'], request['dataset_dir'], request['integration_server'])
+        return open(result_file.path, "rb")
     
 class CAMSEuropeAirQualityForecastsAdaptorForArchivedData(AbstractCdsAdaptor):
     def retrieve(self, request: Request) -> BinaryIO:
-        result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForArchivedData:", 'dummy.txt')
-        return open(result_file_path, "rb")
+        #result_file_path = debug_input(self, request, "From CAMSEuropeAirQualityForecastsAdaptorForArchivedData:", 'dummy.txt')
+        result_file = retrieve_archived(self.context, request['requests'], request['dataset_dir'], request['integration_server'])
+        return open(result_file.path, "rb")
