385e98c7931a826632064ca23cf5f6c05ae9e684
Corvin-Petrut Cobarzan <Corvin-Petrut.Cobarzan@ecmwf.int>
2024-10-02 10:25:08 +0100

    Refactor CAMSEuropeAirQualityForecastsAdaptor:retrieve

--- cads_adaptors/adaptors/cams_regional_fc/__init__.py
+++ cads_adaptors/adaptors/cams_regional_fc/__init__.py
@@ -2,54 +2,16 @@ from cads_adaptors.adaptors.cds import AbstractCdsAdaptor
 from cads_adaptors.exceptions import InvalidRequest
 from cads_adaptors.adaptors.cds import Request
 from typing import BinaryIO
-import json
 
-# CHECKLIST:
-# - LATEST: OK
-# - ARCHIVE: OK
-# - RATE LIMITING: OK
-# - CACHE: OK
-
-def debug_input(adaptor, request, message, output_file):
-    # dumping the config and request
-    with open(output_file, "w") as fp:
-        fp.write("\n\n\n")
-        fp.write(f'{message}')
-        fp.write(json.dumps(adaptor.config))
-        fp.write("\n\n\n")
-        fp.write(json.dumps(request))
-    return output_file
-    
 
 class CAMSEuropeAirQualityForecastsAdaptor(AbstractCdsAdaptor):
     def retrieve(self, request: Request) -> BinaryIO:
         from .cams_regional_fc import new_cams_regional_fc
-        self.context.add_stdout(f"----------> INITIAL REQUEST: {request}")
         request.pop('_in_adaptor_no_cache',None)
-        self.context.add_stdout(f"----------> PURE REQUEST: {request}")
         
-        # Intersect constraints
-        if self.config.get("intersect_constraints", False):
-            requests = self.intersect_constraints(request)
-            # TODO: inhibit the cache avoidance mechanism from normalise_request
-            # when called from intersect_constraints
-            for subrequest in requests:
-                 subrequest.pop('_in_adaptor_no_cache',None)
-            if len(requests) == 0:
-                msg = "Error: no intersection with the constraints."
-                raise InvalidRequest(msg)
-        else:
-            requests = [request]
-        self.context.add_stdout(f"----------> INTERSECTED REQUESTS: {requests}")
-        
-        # apply mapping
-        requests = [self.apply_mapping(request) for request in requests]
-        #self.mapped_request = self.apply_mapping(request)
-        #self.context.add_stdout(f"----------> MAPPED REQUEST: {self.mapped_request}")
-        #ALSO send [self.mapped_request] to new_cams_regional_fc instead of requests
-        # request["type"] = [t.upper() for t in request["type"]]
-        # request["format"] = ['grib']
-        result_file = new_cams_regional_fc(self.context, self.config, self.mapping, requests)
+        self.normalise_request(request)
+
+        result_file = new_cams_regional_fc(self.context, self.config, self.mapping, self.mapped_requests)
             
         return open(result_file.path, "rb")
 
