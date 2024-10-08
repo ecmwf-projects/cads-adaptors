2a08be99e97cd81c722599a35db2b7ff0da8e127
garciam <garciam@predictia.es>
2024-10-08 09:36:18 +0200

    Remove aux variables pattern

--- cads_adaptors/adaptors/cadsobs/adaptor.py
+++ cads_adaptors/adaptors/cadsobs/adaptor.py
@@ -36,29 +36,11 @@ class ObservationsAdaptor(AbstractCdsAdaptor):
 
         self.mapped_request = self.adapt_parameters()
         # Get CDM lite variables as a dict with mandatory, optional and auxiliary
-        # Auxiliary variables are, generally, statistics associated with the mandatory
-        # and/or optional variables. They are identified by matches with the full
-        # variable name, e.g. VAR_standard_deviation, where standard_deviation is the
-        # auxiliary variable. The will be included as additional columns in the output
-        # file.
         cadsobs_client = CadsobsApiClient(obs_api_url, self.context)
         cdm_lite_variables_dict = cadsobs_client.get_cdm_lite_variables()
         cdm_lite_variables = (
             cdm_lite_variables_dict["mandatory"] + cdm_lite_variables_dict["optional"]
         )
-        # Handle auxiliary variables
-        try:
-            aux_var_mapping = cadsobs_client.get_aux_var_mapping(
-                dataset_name, dataset_source
-            )
-        except Exception as e:
-            message = f"KeyError: {dataset_source}"
-            self.context.add_stderr(f"{e}")
-            raise InvalidRequest(message)
-
-        # Note that this mutates mapped_request
-        requested_auxiliary_variables = self.handle_auxiliary_variables(aux_var_mapping)
-        cdm_lite_variables = cdm_lite_variables + list(requested_auxiliary_variables)
         # Get the objects that match the request
         object_urls = cadsobs_client.get_objects_to_retrieve(
             dataset_name, self.mapped_request, size_limit=size_limit
@@ -80,48 +62,6 @@ class ObservationsAdaptor(AbstractCdsAdaptor):
             self.context,
         )
         return open(output_path, "rb")
-
-    def handle_auxiliary_variables(self, aux_var_mapping: dict) -> set[str]:
-        """Remove auxiliary variables from the request and add them as extra fields."""
-        requested_variables = self.mapped_request["variables"].copy()
-        regular_variables = [v for v in requested_variables if v in aux_var_mapping]
-        auxiliary_variables = [
-            v for v in requested_variables if v not in aux_var_mapping
-        ]
-        requested_metadata_fields = set()
-        for regular_variable in regular_variables:
-            for auxvar_dict in aux_var_mapping[regular_variable]:
-                auxvar = auxvar_dict["auxvar"]
-                # Two regular variables may be mapped to the same aux variable
-                # The second check "auxvar in requested_variables" is added to avoid
-                # passing through here twice.
-                if auxvar in auxiliary_variables and auxvar in requested_variables:
-                    metadata_field = auxvar_dict["metadata_name"]
-                    self.context.warning(
-                        f"{auxvar} is an auxiliary variable, it will be included"
-                        f"as an extra {metadata_field} column in the output file, not as a "
-                        f"regular variable."
-                    )
-                    requested_variables.remove(auxvar)
-                    requested_metadata_fields.add(metadata_field)
-        # Check that there are no orphan auxiliary variables without its regular
-        # variable and if any, add the regular variable
-        inverse_aux_var_mapping = {
-            auxvar_dict["auxvar"]: regular_var
-            for regular_var, auxiliary_vars in aux_var_mapping.items()
-            for auxvar_dict in auxiliary_vars
-        }
-        for auxvar in auxiliary_variables:
-            regular_variable = inverse_aux_var_mapping[auxvar]
-            if regular_variable not in requested_variables:
-                self.context.warning(
-                    f"{auxvar} is auxiliary metadata of variable {regular_variable}, "
-                    f"adding ir to the request."
-                )
-                requested_variables.append(regular_variable)
-        self.mapped_request["variables"] = requested_variables
-
-        return requested_metadata_fields
 
     def adapt_parameters(self) -> dict:
         # We need these changes right now to adapt the parameters to what we need
