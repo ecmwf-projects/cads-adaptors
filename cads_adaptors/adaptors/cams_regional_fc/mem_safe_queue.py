32edd14afa8a99000aed38a76bc557c7edccc4e7
Luke Jones <luke.jones@ecmwf.int>
2024-10-16 10:55:35 +0000

    Auto-linting of cams_regional_fc/mem_safe_queue.py

--- cads_adaptors/adaptors/cams_regional_fc/mem_safe_queue.py
+++ cads_adaptors/adaptors/cams_regional_fc/mem_safe_queue.py
@@ -1,19 +1,20 @@
-import os
-import time
-import queue
 import logging
+import os
+import queue
 import threading
+import time
 from tempfile import NamedTemporaryFile
 
 
 class MemSafeQueue(queue.Queue):
     """Subclass of Queue that holds queued items in memory until the queue size
-       hits a limit and then starts temporarily storing them on file instead. It
-       means the queue memory usage will not grow out of control.
+    hits a limit and then starts temporarily storing them on file instead. It
+    means the queue memory usage will not grow out of control.
     """
 
-    def __init__(self, nbytes_max, tmpdir, *args,
-                 logger=logging.getLogger(__name__), **kwargs):
+    def __init__(
+        self, nbytes_max, tmpdir, *args, logger=logging.getLogger(__name__), **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self.nbytes_max = nbytes_max
         self.nbytes = 0
@@ -22,44 +23,46 @@ class MemSafeQueue(queue.Queue):
         self._lock = threading.Lock()
 
         self.stats = {}
-        for k1 in ['queue', 'mem', 'file']:
+        for k1 in ["queue", "mem", "file"]:
             self.stats[k1] = {}
-            for k2 in ['current', 'total', 'max']:
+            for k2 in ["current", "total", "max"]:
                 self.stats[k1][k2] = 0
-        self.stats['iotime'] = 0.
+        self.stats["iotime"] = 0.0
 
     def put(self, item, **kwargs):
-        """Put an item in the queue"""
-
+        """Put an item in the queue."""
         data, fieldinfo = item
-        self.stats['queue']['total'] += 1
-        self.stats['queue']['max'] = max(self.stats['queue']['max'],
-                                         self.qsize())
+        self.stats["queue"]["total"] += 1
+        self.stats["queue"]["max"] = max(self.stats["queue"]["max"], self.qsize())
 
-        self.logger.debug(f'MemSafeQueue: Queue nbytes={self.nbytes}, '
-                          f'in-mem size={self.stats["mem"]["current"]}, '
-                          f'total size={self.qsize()}')
+        self.logger.debug(
+            f'MemSafeQueue: Queue nbytes={self.nbytes}, '
+            f'in-mem size={self.stats["mem"]["current"]}, '
+            f'total size={self.qsize()}'
+        )
 
         # Keep the item in memory or write to file and replace with the path?
         self._lock.acquire()
         if self.nbytes + len(data) <= self.nbytes_max:
             self.nbytes += len(data)
-            self.stats['mem']['total'] += 1
-            self.stats['mem']['current'] += 1
-            self.stats['mem']['max'] = max(self.stats['mem']['current'],
-                                           self.stats['mem']['max'])
+            self.stats["mem"]["total"] += 1
+            self.stats["mem"]["current"] += 1
+            self.stats["mem"]["max"] = max(
+                self.stats["mem"]["current"], self.stats["mem"]["max"]
+            )
             self._lock.release()
         else:
-            self.stats['file']['total'] += 1
-            self.stats['file']['current'] += 1
-            self.stats['file']['max'] = max(self.stats['file']['current'],
-                                            self.stats['file']['max'])
+            self.stats["file"]["total"] += 1
+            self.stats["file"]["current"] += 1
+            self.stats["file"]["max"] = max(
+                self.stats["file"]["current"], self.stats["file"]["max"]
+            )
             self._lock.release()
-            self.logger.debug(f'MemSafeQueue: storing on disk: {fieldinfo!r}')
+            self.logger.debug(f"MemSafeQueue: storing on disk: {fieldinfo!r}")
             t = time.time()
             with NamedTemporaryFile(dir=self.tmpdir, delete=False) as tmp:
                 tmp.write(data)
-            self.stats['iotime'] += (time.time() - t)
+            self.stats["iotime"] += time.time() - t
             item = (tmp.name, fieldinfo)
 
         super().put(item, **kwargs)
@@ -68,28 +71,27 @@ class MemSafeQueue(queue.Queue):
         self.put(item, block=False)
 
     def get(self, **kwargs):
-
         xx, fieldinfo = super().get(**kwargs)
 
         # Received data or a temporary file path?
         if isinstance(xx, bytes):
             data = xx
             self.nbytes -= len(data)
-            self.stats['mem']['current'] -= 1
-            self.logger.debug(f'MemSafeQueue: Queue nbytes={self.nbytes}, '
-                              f'in-mem size={self.stats["mem"]["current"]}, '
-                              f'total size={self.qsize()}')
+            self.stats["mem"]["current"] -= 1
+            self.logger.debug(
+                f'MemSafeQueue: Queue nbytes={self.nbytes}, '
+                f'in-mem size={self.stats["mem"]["current"]}, '
+                f'total size={self.qsize()}'
+            )
         else:
-            self.stats['file']['current'] -= 1
+            self.stats["file"]["current"] -= 1
             t = time.time()
-            with open(xx, 'rb') as tmp:
+            with open(xx, "rb") as tmp:
                 data = tmp.read()
             os.remove(xx)
-            self.stats['iotime'] += (time.time() - t)
+            self.stats["iotime"] += time.time() - t
 
         return (data, fieldinfo)
 
     def get_nowait(self):
         return self.get(block=False)
-
-
