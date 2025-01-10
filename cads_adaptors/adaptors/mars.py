f65faab16aa23142bde86585935c8c4bc899600a
malmans2 <m.almansi@bopen.eu>
2025-01-10 08:54:34 +0100

    rename

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -266,5 +266,5 @@ class MarsCdsAdaptor(cds.AbstractCdsAdaptor):
     def retrieve(self, request: dict[str, Any]) -> BinaryIO:
         with super().retrieve(request) as fp:
             if self.cached_execute_mars.use_cache and fp.name.endswith(".grib"):
-                return cacholote.extra_encoders.FrozenFile(fp.name, "rb")
+                return cacholote.extra_encoders.InPlaceFile(fp.name, "rb")
             return fp
