6b5a1c9f2d5e998409b4a28aaf51883757903d70
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-01-30 09:53:31 +0000

    upload_time hacky message

--- cads_adaptors/tools/area_selector.py
+++ cads_adaptors/tools/area_selector.py
@@ -318,5 +318,5 @@ def area_selector_paths(
                     f"could not convert {path} to xarray; returning the original data"
                 )
                 out_paths.append(path)
-        context.info("Area selection complete", upload_time=time.time() - time0)
+        context.upload_log("Area selection complete", upload_time=time.time() - time0)
     return out_paths
