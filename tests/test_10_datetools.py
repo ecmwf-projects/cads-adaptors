9dc9c29effa5fc559637b5e937968ee725d915f5
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-03-10 08:33:51 +0000

    QA

--- tests/test_10_datetools.py
+++ tests/test_10_datetools.py
@@ -1,10 +1,13 @@
-import pytest
-from datetime import datetime, UTC
+from datetime import UTC, datetime
 from unittest.mock import patch
+
+import pytest
+
+from cads_adaptors.exceptions import InvalidRequest
 
 # Assuming the function is in a module named `embargo_handler`
 from cads_adaptors.tools.date_tools import implement_embargo
-from cads_adaptors.exceptions import InvalidRequest
+
 
 def test_implement_embargo_no_embargo():
     requests = [{"date": ["2025-03-01"]}]
@@ -13,12 +16,14 @@ def test_implement_embargo_no_embargo():
     assert out_requests == requests
     assert cacheable is True
 
+
 def test_implement_embargo_with_days():
     requests = [{"date": ["2025-03-01", datetime.now().strftime("%Y-%m-%d")]}]
     embargo = {"days": 5}
     out_requests, cacheable = implement_embargo(requests, embargo)
     assert out_requests == [{"date": ["2025-03-01"]}]
     assert cacheable is False
+
 
 def test_implement_embargo_with_months():
     requests = [{"date": ["2025-02-01", datetime.now().strftime("%Y-%m-%d")]}]
@@ -27,11 +32,15 @@ def test_implement_embargo_with_months():
     assert out_requests == [{"date": ["2025-02-01"]}]
     assert cacheable is False
 
+
 def test_implement_embargo_all_filtered():
     requests = [{"date": ["2025-03-01", datetime.now().strftime("%Y-%m-%d")]}]
     embargo = {"days": 10}
-    with pytest.raises(InvalidRequest, match="None of the data you have requested is available yet"):
+    with pytest.raises(
+        InvalidRequest, match="None of the data you have requested is available yet"
+    ):
         implement_embargo(requests, embargo)
+
 
 def test_implement_embargo_filter_timesteps():
     requests = [{"date": ["2025-03-01", "2025-03-02"], "time": ["00:00", "12:00"]}]
@@ -51,5 +60,8 @@ def test_implement_embargo_filter_timesteps():
         mock_datetime.now.return_value = datetime(2025, 3, 2, 16, tzinfo=UTC)
         out_requests, cacheable = implement_embargo(requests, embargo)
     assert len(out_requests) == 1
-    assert out_requests[0] == {"date": ["2025-03-01", "2025-03-02"], "time": ["00:00", "12:00"]}
+    assert out_requests[0] == {
+        "date": ["2025-03-01", "2025-03-02"],
+        "time": ["00:00", "12:00"],
+    }
     assert cacheable is True
