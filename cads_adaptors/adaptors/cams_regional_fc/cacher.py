b1c680d0f359838e8a8f56d881147d7be2afe013
Luke Jones <luke.jones@ecmwf.int>
2025-05-02 15:44:23 +0000

    Add abolity to cache from and to disk in cams_regional_fc

--- cads_adaptors/adaptors/cams_regional_fc/cacher.py
+++ cads_adaptors/adaptors/cams_regional_fc/cacher.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import io
 import logging
 import os
+import queue
 import re
 import threading
 import time
@@ -35,6 +36,7 @@ class AbstractCacher:
         self.integration_server = integration_server
         self.logger = logging.getLogger(__name__) if logger is None else logger
         self.no_put = no_put
+        self._close_was_called = False
 
         # The name of a key which, if present in the original request, will be
         # inserted into the field description dictionary when writing to the
@@ -51,7 +53,9 @@ class AbstractCacher:
         self.permanent_fields = permanent_fields
 
     def close(self):
-        pass
+        if self._close_was_called:
+            raise Exception('close() called twice?')
+        self._close_was_called = True
 
     def __enter__(self):
         return self
@@ -61,6 +65,11 @@ class AbstractCacher:
 
     def put(self, req):
         """Write grib fields from a request into the cache."""
+
+        if self._close_was_called:
+            raise Exception('Attempt to write another field after close() '
+                            'called')
+
         # Do not cache sub-area requests when the sub-area is done by the Meteo
         # France backend. With the current code below they would be cached
         # without the area specification in the path and would get confused for
@@ -68,55 +77,7 @@ class AbstractCacher:
         if "north" in req["req"]:
             return
 
-        request = {k: str(v).split(",") for k, v in req["req"].items()}
-
-        # Loop over grib messages in the data
-        data = req["data"].content()
-        assert len(data) > 0
-        try:
-            count = 0
-            for msg in grib_bytes_iterator(data):
-                count += 1
-                if count == 2:
-                    break
-
-                # Figure out the request values that correspond to this field
-                req1field = grib2request(msg)
-
-                # Sanity-check that req1field was part of the request
-                intn, _, _ = hcube_intdiff(
-                    request, {k: [v] for k, v in req1field.items()}
-                )
-                nmatches = count_fields(intn)
-                if nmatches == 0:
-                    raise Exception(
-                        f"Got unexpected field {req1field!r} from request "
-                        + repr(req["req"])
-                    )
-                assert nmatches == 1
-
-                # If self.no_cache_key was in the request then insert it into
-                # req1field. This means it will appear in the cache file name,
-                # which is useful for regression testing. It means multiple
-                # tests that request the same field can share a unique no-cache
-                # value so the field is retrieved from the backend the first
-                # time but from cache on subsequent attempts.
-                if self.no_cache_key in req["req"]:
-                    req1field[self.no_cache_key] = req["req"][self.no_cache_key]
-
-                # Convert the message to pure binary data and write to cache
-                self._write_field(codes_get_message(msg), req1field)
-
-        except Exception:
-            # Temporary code for debugging
-            # from random import randint
-            # from datetime import datetime
-            # unique_string = datetime.now().strftime('%Y%m%d%H%M%S.') + \
-            #    str(randint(0,99999))
-            # with open(f'{self.temp_cache_root}/{unique_string}'
-            #          '.actually_bad.grib', 'wb') as f:
-            #    f.write(data)
-            raise
+        self._write_fields(req)
 
     def get(self, req):
         """Get a file from the cache or raise NotInCache if it doesn't exist."""
@@ -132,8 +93,8 @@ class AbstractCacher:
         """Return the URL of the specified field in the cache."""
         raise Exception("Needs to be overloaded by a child class")
 
-    def _write_field(self, msg, req1field):
-        """Write a field to the cache."""
+    def _write_fields(self, req):
+        """Write fields to the cache."""
         raise Exception("Needs to be overloaded by a child class")
 
     def _cache_permanently(self, field):
@@ -201,6 +162,61 @@ class AbstractCacher:
 
         return f"{dir}/" + self._templates[keys].render(fieldinfo)
 
+    def _field_iter(self, req):
+        """Yield a descriptive dict and the associated data for each of the
+        fields in req
+        """
+
+        # Archive-backend requests can retrieve a hypercube of fields. Parse the
+        # comma-separated lists into proper lists.
+        request = {k: str(v).split(",") for k, v in req["req"].items()}
+
+        # The req dict may contain the field data in memory or a path to a file
+        # that contains the data. Get the data.
+        if "data" in req:
+            data = req["data"]
+            if not isinstance(data, bytes):
+                # Get bytes array from a ResponseObject
+                data = data.content()
+        else:
+            with open(req["path"], "rb") as f:
+                data = f.read()
+        assert len(data) > 0
+
+        # If there is one field in the request we can assume that the data is
+        # of that single field, so we can make this shortcut.
+        if count_fields(request) == 1:
+            yield (req["req"], data)
+            return
+
+        # Loop over the grib fields
+        for msg in grib_bytes_iterator(data):
+
+            # Figure out the request values that correspond to this field
+            fieldinfo = grib2request(msg)
+
+            # Sanity-check that fieldinfo was part of the request
+            intn, _, _ = hcube_intdiff(
+                request, {k: [v] for k, v in fieldinfo.items()}
+            )
+            nmatches = count_fields(intn)
+            if nmatches == 0:
+                raise Exception(
+                    f"Got unexpected field {fieldinfo!r} from request "
+                    + repr(req["req"])
+                )
+            assert nmatches == 1
+
+            # If self.no_cache_key was in the request then insert it into
+            # fieldinfo. This means it will appear in the cache file name,
+            # which is useful for regression testing. It means multiple
+            # tests that request the same field can share a unique no-cache
+            # value so the field is retrieved from the backend the first
+            # time but from cache on subsequent attempts.
+            if self.no_cache_key in req["req"]:
+                fieldinfo[self.no_cache_key] = req["req"][self.no_cache_key]
+
+            yield (fieldinfo, codes_get_message(msg))
 
 class AbstractAsyncCacher(AbstractCacher):
     """Augment the AbstractCacher class to add asynchronous cache puts. This
@@ -234,7 +250,6 @@ class AbstractAsyncCacher(AbstractCacher):
         self.timeout = 300 if timeout is None else timeout
         self._lock1 = threading.Lock()
         self._lock2 = threading.Lock()
-        self._qclosed = False
         self._templates = {}
         self._futures = []
         self._start_time = None
@@ -246,23 +261,25 @@ class AbstractAsyncCacher(AbstractCacher):
         """Start the threads that will do the remote copies."""
         exr = concurrent.futures.ThreadPoolExecutor(max_workers=self.nthreads)
         self._start_time = time.time()
-        self._futures = [exr.submit(self._copier) for _ in range(self.nthreads)]
+        self._waiting = [False] * self.nthreads
+        self._futures = [exr.submit(self._copier, i)
+                         for i in range(self.nthreads)]
         exr.shutdown(wait=False)
 
     def close(self):
         """Close the queue and wait for threads to finish"""
+        super().close()
+
         if self._futures:
-            # Close the queue
-            self._queue.put((b"", None))
-            qclose_time = time.time()
+            close_time = time.time()
 
             # Wait for each thread to complete and check if any raised an
             # exception
             for future in self._futures:
                 try:
                     exc = future.exception(
-                        timeout=max(self.timeout-(qclose_time-self._start_time),
-                                    60))
+                        timeout=max(self.timeout-(time.time()-self._start_time),
+                                    0))
                 except TimeoutError:
                     # This won't cancel running futures, but it's better than
                     # nothing
@@ -277,39 +294,78 @@ class AbstractAsyncCacher(AbstractCacher):
             now = time.time()
             summary["time_secs"] = {
                 "elapsed": now - self._start_time,
-                "drain": now - qclose_time,
+                "drain": now - close_time,
                 "io": iotime,
             }
             self.logger.info(f"MemSafeQueue summary: {summary!r}")
 
-    def _write_field(self, data, fieldinfo):
-        """Asynchronously copy the bytes data to the specified file on
-        specified host.
-        """
+    def _write_fields(self, req):
+        """Asynchronously cache fields"""
+        self._queue.put(req)
         # Start the copying thread if not done already
         with self._lock1:
             if not self._futures:
                 self._start_copy_threads()
 
-        self._queue.put((data, fieldinfo))
+    def _copier(self, ithread):
+        """Thread to actually copy the data. There will be several of these
+           running at the same time."""
 
-    def _copier(self):
-        """Thread to actually copy the data."""
+        # Loop over items in the queue
         while True:
-            # This lock is required so that only 1 thread marks the queue as
-            # closed
-            with self._lock2:
-                if self._qclosed:
-                    break
-                data, fieldinfo = self._queue.get()
-                self._qclosed = fieldinfo is None
-            if self._qclosed:
+
+            # Get an item from the queue
+            req = self._queue_get(ithread)
+            if req is None:
                 break
-            self._write_field_sync(data, fieldinfo)
+
+            # It may be that this item represents multiple fields, in which case
+            # we want to cache them in parallel for speed, so put every item
+            # apart from the last back in the queue and only cache the last
+            # one. Note: this code block could be commented and everything would
+            # still work, just slower for multi-field inputs.
+            prev = None
+            for fieldinfo, data in self._field_iter(req):
+                if prev:
+                    self._queue.put(prev)
+                prev = {"req": fieldinfo,
+                        "data": data}
+            req = prev
+
+            self._write_fields_sync(req)
 
         n = self._queue.qsize()
         if n > 0:
             raise Exception(f"{n} unconsumed items in queue")
+
+    def _queue_get(self, ithread):
+        """Return an item from the queue or None if there are no more items"""
+
+        # Loop to poll for a new item
+        while time.time() - self._start_time < self.timeout:
+
+            closed = self._close_was_called
+            self._waiting[ithread] = False
+            with self._lock2:
+                try:
+                    req = self._queue.get(block=False)
+                except queue.Empty:
+                    # There will be no new items if self.close() has been called
+                    # and all other threads are waiting inside this same loop
+                    self._waiting[ithread] = True
+                    if closed and all(self._waiting):
+                        return None
+                else:
+                    return req
+
+            time.sleep(0.1)
+
+    def _write_fields_sync(self, req):
+        """Write the fields to the appropriate cache location."""
+
+        # Loop over grib messages in the data
+        for fieldinfo, data in self._field_iter(req):
+            self._write_1field_sync(data, fieldinfo)
 
 
 class CacherS3(AbstractAsyncCacher):
@@ -338,10 +394,9 @@ class CacherS3(AbstractAsyncCacher):
             if not bkt.creation_date:
                 bkt = self.client.create_bucket(Bucket=self._bucket)
 
-    def _write_field_sync(self, data, fieldinfo):
-        """Write the data described by fieldinfo to the appropriate cache
-        location.
-        """
+    def _write_1field_sync(self, data, fieldinfo):
+        """Write one field to the appropriate cache location."""
+
         local_object = io.BytesIO(data)
         remote_path = self._cache_file_path(fieldinfo)
 
@@ -389,7 +444,22 @@ class CacherS3(AbstractAsyncCacher):
         self.client.delete_object(Bucket=self._bucket, Key=remote_path)
 
 
-class CacherS3AndFile(CacherS3):
+class CacherDisk(AbstractAsyncCacher):
+
+    def __init__(self, *args, field2path=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.field2path = field2path
+
+    def _write_1field_sync(self, data, fieldinfo):
+
+        path = self.field2path(fieldinfo)
+        self.logger.info(f"Caching {fieldinfo} to {path}")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "wb") as f:
+            f.write(data)
+
+
+class CacherS3AndDisk(CacherS3):
     """Sub-class of CacherS3 to cache not only to an S3 bucket but to a local
     file as well. Failures to write to the S3 bucket are logged but considered
     non-fatal so as not to disrupt the caching to file, which is considered
@@ -401,7 +471,7 @@ class CacherS3AndFile(CacherS3):
         self.field2path = field2path
         self.s3_errors = []
 
-    def _write_field_sync(self, data, fieldinfo):
+    def _write_1fiels_sync(self, data, fieldinfo):
 
         # Write to a local path?
         if self.field2path:
@@ -413,7 +483,7 @@ class CacherS3AndFile(CacherS3):
 
         # Write to the S3 bucket
         try:
-            super()._write_field_sync(data, fieldinfo)
+            super()._write_fields_sync(data, fieldinfo)
         except Exception as e:
             self._log_s3_error("S3 write", e)
 
--- cads_adaptors/adaptors/cams_regional_fc/mem_safe_queue.py
+++ cads_adaptors/adaptors/cams_regional_fc/mem_safe_queue.py
@@ -1,8 +1,12 @@
 import logging
 import os
+import pickle
 import queue
 import threading
 import time
+from collections import deque
+from itertools import chain
+from sys import getsizeof
 from tempfile import NamedTemporaryFile
 
 
@@ -29,9 +33,9 @@ class MemSafeQueue(queue.Queue):
 
     def put(self, item, **kwargs):
         """Put an item in the queue."""
-        data, fieldinfo = item
-        self.stats["queue"]["total"] += 1
-        self.stats["queue"]["max"] = max(self.stats["queue"]["max"], self.qsize())
+        with self._lock:
+            self.stats["queue"]["total"] += 1
+            self.stats["queue"]["max"] = max(self.stats["queue"]["max"], self.qsize())
 
         self.logger.debug(
             f'MemSafeQueue: Queue nbytes={self.nbytes}, '
@@ -40,58 +44,112 @@ class MemSafeQueue(queue.Queue):
         )
 
         # Keep the item in memory or write to file and replace with the path?
-        self._lock.acquire()
-        if self.nbytes + len(data) <= self.nbytes_max:
-            self.nbytes += len(data)
-            self.stats["mem"]["total"] += 1
-            self.stats["mem"]["current"] += 1
-            self.stats["mem"]["max"] = max(
-                self.stats["mem"]["current"], self.stats["mem"]["max"]
-            )
-            self._lock.release()
+        size = total_size(item)
+        if self.nbytes + size <= self.nbytes_max:
+            with self._lock:
+                self.nbytes += size
+            kstats = "mem"
         else:
-            self.stats["file"]["total"] += 1
-            self.stats["file"]["current"] += 1
-            self.stats["file"]["max"] = max(
-                self.stats["file"]["current"], self.stats["file"]["max"]
-            )
-            self._lock.release()
-            self.logger.debug(f"MemSafeQueue: storing on disk: {fieldinfo!r}")
             t = time.time()
-            if self.tmpdir:
-                os.makedirs(self.tmpdir, exist_ok=True)
-            with NamedTemporaryFile(dir=self.tmpdir, delete=False) as tmp:
-                tmp.write(data)
-            self.stats["iotime"] += time.time() - t
-            item = (tmp.name, fieldinfo)
+            item = _Pickle(item, self.tmpdir)
+            self.logger.debug(f"MemSafeQueue: stored {size}B item on disk")
+            with self._lock:
+                self.stats["iotime"] += time.time() - t
+            kstats = "file"
 
-        super().put(item, **kwargs)
+        # Update summary stats
+        with self._lock:
+            self.stats[kstats]["total"] += 1
+            self.stats[kstats]["current"] += 1
+            self.stats[kstats]["max"] = max(
+                self.stats[kstats]["current"], self.stats[kstats]["max"]
+            )
+
+        super().put((item, size), **kwargs)
 
     def put_nowait(self, item, **kwargs):
         self.put(item, block=False)
 
     def get(self, **kwargs):
-        xx, fieldinfo = super().get(**kwargs)
+        item, size = super().get(**kwargs)
 
-        # Received data or a temporary file path?
-        if isinstance(xx, bytes):
-            data = xx
-            self.nbytes -= len(data)
-            self.stats["mem"]["current"] -= 1
+        # Received original item or a path to a temp pickle file?
+        if isinstance(item, _Pickle):
+            t = time.time()
+            item = item.unpickle()
+            with self._lock:
+                self.stats["file"]["current"] -= 1
+                self.stats["iotime"] += time.time() - t
+        else:
+            with self._lock:
+                self.nbytes -= size
+                self.stats["mem"]["current"] -= 1
             self.logger.debug(
                 f'MemSafeQueue: Queue nbytes={self.nbytes}, '
                 f'in-mem size={self.stats["mem"]["current"]}, '
                 f'total size={self.qsize()}'
-            )
-        else:
-            self.stats["file"]["current"] -= 1
-            t = time.time()
-            with open(xx, "rb") as tmp:
-                data = tmp.read()
-            os.remove(xx)
-            self.stats["iotime"] += time.time() - t
+            )            
 
-        return (data, fieldinfo)
+        return item
 
     def get_nowait(self):
         return self.get(block=False)
+
+
+class _Pickle:
+    """Class to pickle & unpickle an object to/from a temporary file"""
+
+    def __init__(self, item, tmpdir):
+        if tmpdir:
+            os.makedirs(tmpdir, exist_ok=True)
+        with NamedTemporaryFile(dir=tmpdir, delete=False) as f:
+            self.path = f.name
+            pickle.dump(item, f)
+
+    def unpickle(self):
+        with open(self.path, "rb") as f:
+            item = pickle.load(f)
+        os.remove(self.path)
+        return item
+
+
+# This function is a copy-pasted recipe from the internet
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o))
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
