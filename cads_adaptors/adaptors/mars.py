cfcf744da464658c07e76482199316ec98dab8ca
Gionata <gionata.biavati@ecmwf.int>
2025-05-23 13:44:36 +0000

    Refactor error handling in execute_mars to always raise MarsRuntimeError for file existence issues

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -159,10 +159,8 @@ def execute_mars(
         context.add_user_visible_error(message=error_message)
 
         error_message += f"Exception: {reply.error}\n"
-        if filesize > 0:
-            context.add_user_visible_error(message=f"Not rising this error { error_message } because the file exists")
-        else:
-            raise MarsRuntimeError(error_message)
+    
+        raise MarsRuntimeError(error_message)
 
     if not filesize:
         error_message = (
