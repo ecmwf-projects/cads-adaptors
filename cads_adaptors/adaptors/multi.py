c0876499c3ee54debea8f26eb32aa81f23355d0e
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-07-28 17:29:28 +0100

    multi-adaptor intersect

--- cads_adaptors/adaptors/multi.py
+++ cads_adaptors/adaptors/multi.py
@@ -197,9 +197,9 @@ class MultiAdaptor(AbstractCdsAdaptor):
         # We do not want to create a very large number of sub-adaptors
         self.mapped_request = merge_requests(self.mapped_requests)
 
-        self.context.debug(f"MultiAdaptor, full_request: {self.mapped_request}")
+        self.context.debug(f"MultiAdaptor, full_request: {request}")
 
-        sub_adaptors = self.split_adaptors(self.mapped_request)
+        sub_adaptors = self.split_adaptors(request)
 
         paths: list[str] = []
         exception_logs: dict[str, str] = {}
