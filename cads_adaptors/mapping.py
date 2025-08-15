a0112d1a7cf4334b8c2bd6ff3018b9da1b13c766
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2025-08-15 14:51:43 +0100

    Feature/area as mapping before intersect constraints (#306)
    * area-mapping before intersection
    * remove only one safety net now it can be used with intersect_constraints

--- cads_adaptors/mapping.py
+++ cads_adaptors/mapping.py
@@ -235,6 +235,73 @@ def expand_dates(r, request, date, year, month, day, date_format):
                     del r[k]
 
 
+def area_as_mapping(
+    request: dict[str, Any], mapping: dict[str, Any], context: Context = Context()
+) -> dict[str, Any]:
+    options = mapping.get("options", {})
+    # Check there is area and area_mapping
+    if "area" not in request or "area_as_mapping" not in options:
+        return request
+
+    # Copy request and pop out the area
+    out_request = copy.deepcopy(request)
+    area = out_request.pop("area")
+
+    # Extract area mapping information from options
+    area_mapping: list[dict[str, Any]] = options["area_as_mapping"]
+
+    context.debug(f"Mapping area {area!r} using area_as_mapping: {area_mapping!r}")
+    if not isinstance(area_mapping, list):
+        raise exceptions.CdsConfigurationError(
+            "Invalid area_as_mapping option, should be a list"
+        )
+
+    try:
+        area = [float(coord) for coord in area]
+    except (ValueError, TypeError):
+        raise exceptions.InvalidRequest(
+            f"Invalid area provided: {area!r}. "
+            "Should be a list of four numeric values."
+        )
+
+    mapped_area_values: dict[str, list[str | int | float]] = {}
+    for latlon_mapping in area_mapping:
+        try:
+            _lat = float(latlon_mapping["latitude"])
+            _lon = float(latlon_mapping["longitude"])
+        except (KeyError, ValueError):
+            # Ignore incorrectly configured area_as_mapping elements
+            context.warning(
+                f"Invalid area_as_mapping element: {latlon_mapping!r}, "
+                "should contain 'latitude' and 'longitude' keys with float values."
+            )
+            continue
+
+        remaining = {
+            k: v
+            for k, v in latlon_mapping.items()
+            if k not in ("latitude", "longitude")
+        }
+
+        if _lat < area[0] and _lon > area[1] and _lat > area[2] and _lon < area[3]:
+            for _key, _value in remaining.items():
+                if _key not in mapped_area_values:
+                    mapped_area_values[_key] = ensure_list(_value)
+                else:
+                    mapped_area_values[_key].extend(ensure_list(_value))
+
+    for key, values in mapped_area_values.items():
+        if key in out_request:
+            if isinstance(out_request[key], list):
+                out_request[key].extend(values)
+            else:
+                out_request[key] = [out_request[key]] + values
+        else:
+            out_request[key] = values
+
+    return out_request
+
+
 def apply_mapping(
     request: dict[str, Any], mapping: dict[str, Any], context: Context = Context()
 ) -> dict[str, Any]:
@@ -266,66 +333,6 @@ def apply_mapping(
                 request[name] = [remap.get(v, v) for v in oldvalues]
             else:
                 request[name] = remap.get(oldvalues, oldvalues)
-
-    if (area_mapping := options.get("area_as_mapping")) and (
-        area := request.get("area")
-    ):
-        context.debug(f"Mapping area {area!r} using area_as_mapping: {area_mapping!r}")
-        if not isinstance(area_mapping, list):
-            raise exceptions.CdsConfigurationError(
-                "Invalid area_as_mapping option, should be a list"
-            )
-
-        try:
-            area = [float(coord) for coord in area]
-        except (ValueError, TypeError):
-            raise exceptions.InvalidRequest(
-                f"Invalid area provided: {area!r}. "
-                "Should be a list of four numeric values."
-            )
-
-        mapped_area_values: dict[str, list[str | int | float]] = {}
-        for latlon_mapping in area_mapping:
-            try:
-                _lat = float(latlon_mapping["latitude"])
-                _lon = float(latlon_mapping["longitude"])
-            except (KeyError, ValueError):
-                # Ignore incorrectly configured area_as_mapping elements
-                context.warning(
-                    f"Invalid area_as_mapping element: {latlon_mapping!r}, "
-                    "should contain 'latitude' and 'longitude' keys with float values."
-                )
-                continue
-
-            remaining = {
-                k: v
-                for k, v in latlon_mapping.items()
-                if k not in ("latitude", "longitude")
-            }
-            if len(remaining) != 1:
-                # Ignore incorrectly configured area_as_mapping elements
-                context.warning(
-                    f"Invalid area_as_mapping element: {latlon_mapping!r}, "
-                    "should contain exactly one additional key."
-                )
-                continue
-
-            if _lat < area[0] and _lon > area[1] and _lat > area[2] and _lon < area[3]:
-                for _key, _value in remaining.items():
-                    if _key not in mapped_area_values:
-                        mapped_area_values[_key] = ensure_list(_value)
-                    else:
-                        mapped_area_values[_key].extend(ensure_list(_value))
-
-        for key, values in mapped_area_values.items():
-            if key in request:
-                if isinstance(request[key], list):
-                    request[key].extend(values)
-                else:
-                    request[key] = [request[key]] + values
-            else:
-                request[key] = values
-        del request["area"]
 
     r = {}
 
--- tests/test_10_mapping.py
+++ tests/test_10_mapping.py
@@ -204,17 +204,14 @@ def test_area_as_mapping_applied_correctly():
     adaptor_mapping = {
         "options": {
             "area_as_mapping": [
-                {
-                    "latitude": 55,
-                    "longitude": 0,
-                    "country": "UK",
-                }
+                {"latitude": 55, "longitude": 0, "country": "UK", "source": "satellite"}
             ]
         }
     }
-    result = mapping.apply_mapping(request, adaptor_mapping)
+    result = mapping.area_as_mapping(request, adaptor_mapping)
 
     assert result["country"] == ["UK"]
+    assert result["source"] == ["satellite"]
     assert "area" not in result  # Area should not be in the result
     assert "latitude" not in result
     assert "longitude" not in result
@@ -230,7 +227,7 @@ def test_area_as_mapping_merges_multiple_matches():
             ]
         }
     }
-    result = mapping.apply_mapping(request, adaptor_mapping)
+    result = mapping.area_as_mapping(request, adaptor_mapping)
 
     assert sorted(result["tag"]) == ["A", "B"]
     assert "area" not in result  # Area should not be in the result
@@ -248,7 +245,7 @@ def test_area_as_mapping_raises_if_not_list():
         }
     }
     with pytest.raises(exceptions.CdsConfigurationError):
-        mapping.apply_mapping(request, adaptor_mapping)
+        mapping.area_as_mapping(request, adaptor_mapping)
 
 
 def test_area_as_mapping_ignore_incorrect_elements():
@@ -259,16 +256,10 @@ def test_area_as_mapping_ignore_incorrect_elements():
                 {"latitude": 55, "longitude": 0, "country": "UK"},  # Correct element
                 {"latitude": 53, "longitude": -8, "country": "IE"},  # Correct element
                 {"longitude": 0, "country": "FR"},  # Incorrect element, no latitude
-                {
-                    "latitude": 55,
-                    "longitude": 0,
-                    "country": "DE",
-                    "another": "value",
-                },  # Incorrect element, too many keys
             ]
         }
     }
-    result = mapping.apply_mapping(request, adaptor_mapping)
+    result = mapping.area_as_mapping(request, adaptor_mapping)
     assert sorted(result["country"]) == [
         "IE",
         "UK",
@@ -285,7 +276,7 @@ def test_area_as_mapping_does_nothing_if_no_match():
             "area_as_mapping": [{"latitude": 50, "longitude": 5, "country": "Nowhere"}]
         }
     }
-    result = mapping.apply_mapping(request, adaptor_mapping)
+    result = mapping.area_as_mapping(request, adaptor_mapping)
     assert "country" not in result
 
 
@@ -296,5 +287,5 @@ def test_area_as_mapping_merges_with_existing_keys():
             "area_as_mapping": [{"latitude": 55, "longitude": 0, "source": "satellite"}]
         }
     }
-    result = mapping.apply_mapping(request, adaptor_mapping)
+    result = mapping.area_as_mapping(request, adaptor_mapping)
     assert result["source"] == ["ground", "satellite"]
