29974b15373fa325546344eb6dd7dd11bd46791c
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-09-01 11:12:54 +0100

    handle None for data_format

--- cads_adaptors/adaptors/mars.py
+++ cads_adaptors/adaptors/mars.py
@@ -202,7 +202,7 @@ class MarsCdsAdaptor(cds.AbstractCdsAdaptor):
 
         data_formats = [req.pop("data_format", None) for req in self.mapped_requests]
         data_formats = list(set(data_formats))
-        if len(data_formats) != 1:
+        if len(data_formats) != 1 or data_formats[0] is None:
             # It should not be possible to reach here, if it is, there is a problem.
             raise CdsConfigError(
                 "Something has gone wrong in preparing your request, "
