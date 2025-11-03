b66a82fd46cf65486e5e11d9bd620ec82739a671
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-11-03 08:52:38 +0000

    fix, with tests

---
+++ tests/test_50_url_adaptor.py
@@ -0,0 +1,61 @@
+import logging
+import pathlib
+from datetime import datetime, timedelta
+from typing import Any, Type
+
+import numpy as np
+import pandas as pd
+import pytest
+import xarray as xr
+
+from cads_adaptors import UrlCdsAdaptor
+from cads_adaptors.adaptors import Context
+# from cads_adaptors.exceptions import ArcoDataLakeNoDataError, InvalidRequest
+
+
+def test_url_pre_mapping_modifications():
+    adaptor = UrlCdsAdaptor(
+        config={},
+        form=[],
+        context=Context(),
+    )
+
+    request = {
+        "format": "zip",
+        "area": [30, 20, 10, 40],
+        "other_param": "value",
+    }
+
+    modified_request = adaptor.pre_mapping_modifications(request)
+
+    assert "format" not in modified_request
+    assert "area" not in modified_request
+    assert adaptor.download_format == "zip"
+    assert adaptor.area == [30, 20, 10, 40]
+    assert modified_request["other_param"] == "value"
+
+
+def test_url_pre_mapping_modifications_area_as_mapping():
+    adaptor = UrlCdsAdaptor(
+        mapping={
+            "options": {
+                "area_as_mapping": [
+                    {"id": "a", "latitude": 15, "longitude": 25},
+                ]
+            }
+        },
+        form=[],
+        context=Context(),
+    )
+
+    request = {
+        "format": "zip",
+        "area": [30, 20, 10, 40],
+        "other_param": "value",
+    }
+
+    modified_request = adaptor.pre_mapping_modifications(request)
+
+    assert "area" not in modified_request
+    assert "id" in modified_request
+    assert "a" in modified_request["id"]
